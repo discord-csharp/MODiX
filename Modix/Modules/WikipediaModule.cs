04e16c4f99ab2cb8fcd4ff37a4df70feebe79cad
mariocatch <mariocatch@gmail.com>
2017-04-14 00:07:12 -0400

    Clean up WikipediaModule, reduce nesting, add comments

--- Modix/Modules/WikipediaModule.cs
+++ Modix/Modules/WikipediaModule.cs
@@ -16,53 +16,50 @@ namespace Modix.Modules
         {
             var response = await new WikipediaService().GetWikipediaResultsAsync(phrase);
 
-            if (response != null && response.Query != null && response.Query.Pages.Any())
+            // Empty response.
+            if (response == null || response.Query == null || !response.Query.Pages.Any())
             {
-                var messageBuilder = new StringBuilder();
-                foreach (var page in response.Query.Pages.Values)
-                {
-                    if (string.IsNullOrWhiteSpace(page.Extract))
-                    {
-                        await ReplyAsync($"Failed to find anything for `!wiki {phrase}`.");
-                    }
-                    else
-                    {
-                        messageBuilder.AppendLine(page.Extract);
-                    }
-                }
+                await ReplyAsync($"Failed to find anything for `!wiki {phrase}`.");
+            }
 
-                var message = messageBuilder.ToString();
+            // Construct results into one string (use StringBuilder for concat speed).
+            var messageBuilder = new StringBuilder();
+            response.Query.Pages.Values.ToList().ForEach(p => messageBuilder.AppendLine(p.Extract));
+            var message = messageBuilder.ToString();
 
-                if (message.Length > 0)
+            // Sometimes we get here and there's no message, just double check.
+            if (message.Length == 0)
+            {
+                await ReplyAsync($"Failed to find anything for `!wiki {phrase}`.");
+            }
+
+            // Discord has a limit on channel message size... so this accounts for that.
+            if (message.Length > DiscordConfig.MaxMessageSize)
+            {
+                // How many batches do we need to send?
+                // IE: 5000 / 2000 = 2.5
+                // Round up = 3
+                decimal batchCount = Math.Ceiling(decimal.Divide(message.Length, DiscordConfig.MaxMessageSize));
+                int cursor = 0;
+                for (var i = 0; i < batchCount; i++)
                 {
-                    if (message.Length > DiscordConfig.MaxMessageSize)
-                    {
-                        // How many batches do we need to send?
-                        // IE: 5000 / 2000 = 2.5
-                        // Round up = 3
-                        decimal batchCount = Math.Ceiling(decimal.Divide(message.Length, DiscordConfig.MaxMessageSize));
-                        int cursor = 0;
-                        for (var i = 0; i < batchCount; i++)
-                        {
-                            var builder = new EmbedBuilder()
-                               .WithColor(new Color(95, 186, 125))
-                               .WithTitle($"Results for {phrase} (pt {i + 1})")
-                               .WithDescription(message.Substring(cursor, (i == batchCount - 1) ? message.Length - cursor : DiscordConfig.MaxMessageSize));
-                            builder.Build();
-                            await ReplyAsync("", embed: builder);
-                            cursor += DiscordConfig.MaxMessageSize;
-                        }
-                    }
-                    else
-                    {
-                        var builder = new EmbedBuilder()
-                            .WithColor(new Color(95, 186, 125))
-                            .WithTitle($"Results for {phrase}")
-                            .WithDescription(message);
-                        builder.Build();
-                        await ReplyAsync("", embed: builder);
-                    }
+                    var builder = new EmbedBuilder()
+                       .WithColor(new Color(95, 186, 125))
+                       .WithTitle($"Results for {phrase} (pt {i + 1})")
+                       .WithDescription(message.Substring(cursor, (i == batchCount - 1) ? message.Length - cursor : DiscordConfig.MaxMessageSize));
+                    builder.Build();
+                    await ReplyAsync("", embed: builder);
+                    cursor += DiscordConfig.MaxMessageSize;
                 }
+            }
+            else
+            {
+                var builder = new EmbedBuilder()
+                    .WithColor(new Color(95, 186, 125))
+                    .WithTitle($"Results for {phrase}")
+                    .WithDescription(message);
+                builder.Build();
+                await ReplyAsync("", embed: builder);
             }
         }
     }
