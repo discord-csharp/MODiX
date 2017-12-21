f38ba54212f9b57868cb47bb0cb8321bc9dc5869
Patrick Klaeren <patrick@totalren.com>
2017-12-21 17:48:13 +0000

    Added !quote command (#8)
    * Added !quote command
    
    Added ability to invoke !quote and fetch the relevant Discord message with its ID
    
    * Scoped QuoteService
    
    "Non-statified" QuoteService, now scoped in the DI container

---
+++ Modix/Services/Quote/QuoteService.cs
@@ -0,0 +1,43 @@
+﻿using Discord;
+
+namespace Modix.Services.Quote
+{
+    public interface IQuoteService
+    {
+        EmbedBuilder BuildQuoteEmbed(IMessage message);
+    }
+
+    public class QuoteService : IQuoteService
+    {
+        public EmbedBuilder BuildQuoteEmbed(IMessage message)
+        {
+            return new EmbedBuilder()
+                .WithAuthor(message.Author)
+                .WithDescription(message.Content)
+                .WithFooter(GetPostedMeta(message))
+                .WithColor(new Color(95, 186, 125));
+        }
+
+        private static string GetPostedMeta(IMessage message)
+            => string.Format("{0:dddd, dd}{1} {0:MMMM yyyy} at {0:HH:mm}, in #{2}", message.Timestamp, GetDaySuffix(message.Timestamp.Day), message.Channel.Name);
+
+        private static string GetDaySuffix(int day)
+        {
+            switch (day)
+            {
+                case 1:
+                case 21:
+                case 31:
+                    return "st";
+                case 2:
+                case 22:
+                    return "nd";
+                case 3:
+                case 23:
+                    return "rd";
+                default:
+                    return "th";
+            }
+        }
+    }
+}
