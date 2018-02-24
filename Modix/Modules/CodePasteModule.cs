fc96711b3464fb46050eb34e04532cc3f4ebd540
Cisien <ccurwick@exoronet.net>
2018-02-24 10:51:07 -0800

    Handle hastebin failure in the CodePaste module

--- Modix/Modules/CodePasteModule.cs
+++ Modix/Modules/CodePasteModule.cs
@@ -1,5 +1,6 @@
 ﻿using Discord.Commands;
 using Modix.Services.AutoCodePaste;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace Modix.Modules
@@ -9,17 +10,37 @@ namespace Modix.Modules
     {
         private CodePasteService _service;
 
-        public CodePasteModule(CodePasteService service) {
+        public CodePasteModule(CodePasteService service)
+        {
             _service = service;
-    }
+        }
+
         [Command("paste"), Summary("Paste the rest of your message to the internet, and return the URL.")]
         public async Task Run([Remainder] string code)
         {
-            string url = await _service.UploadCode(Context.Message, code);
-            var embed = _service.BuildEmbed(Context.User, code, url);
+            string url = null;
+            string error = null;
+            try
+            {
+                url = await _service.UploadCode(Context.Message, code);
+            }
+            catch (WebException ex)
+            {
+                url = null;
+                error = ex.Message;
+            }
 
-            await ReplyAsync(Context.User.Mention, false, embed);
-            await Context.Message.DeleteAsync();
+            if (url != null)
+            {
+                var embed = _service.BuildEmbed(Context.User, code, url);
+
+                await ReplyAsync(Context.User.Mention, false, embed);
+                await Context.Message.DeleteAsync();
+            }
+            else
+            {
+                await ReplyAsync(error);
+            }
         }
     }
 }
