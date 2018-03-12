64de54cdfde673940a2a7d938cd02bbce3adf1e9
Charles Milette <charles.milette@gmail.com>
2018-03-12 19:24:02 -0400

    Add fox command
    The more cute things the better

---
+++ Modix/Modules/FoxModule.cs
@@ -0,0 +1,33 @@
+﻿using Discord.Commands;
+using Modix.Services.Fox;
+using System;
+using System.Threading.Tasks;
+
+namespace Modix.Modules
+{
+    [Summary("Fox Related Commands")]
+    public class FoxModule : ModuleBase
+    {
+        [Command("fox", RunMode = RunMode.Async)]
+        public async Task Fox()
+        {
+            string imageUrl;
+            try
+            {
+                imageUrl = await new FoxService().GetFoxPicture();
+            }
+            catch (TaskCanceledException)
+            {
+                await ReplyAsync("Couldn't get a fox picture in time :(");
+                return;
+            }
+            catch (Exception exc)
+            {
+                await ReplyAsync($"Couldn't get a fox picture: {exc.Message}");
+                return;
+            }
+
+            await ReplyAsync(imageUrl);
+        }
+    }
+}
---
+++ Modix/Services/Fox/FoxService.cs
@@ -0,0 +1,39 @@
+﻿namespace Modix.Services.Fox
+{
+    using System;
+    using System.Net;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+
+    public class FoxService
+    {
+        private const string FoxApiUrl = "https://giraffeduck.com/api/fox/direct";
+
+        public async Task<string> GetFoxPicture()
+        {
+            var handler = new HttpClientHandler
+            {
+                AllowAutoRedirect = false
+            };
+
+            var client = new HttpClient(handler)
+            {
+                Timeout = TimeSpan.FromSeconds(5)
+            };
+
+            using (client)
+            {
+                var response = await client.GetAsync(FoxApiUrl);
+
+                if (response.StatusCode == HttpStatusCode.Redirect)
+                {
+                    return response.Headers.Location.AbsoluteUri;
+                }
+                else
+                {
+                    throw new Exception("Invalid data received from API.");
+                }
+            }
+        }
+    }
+}
