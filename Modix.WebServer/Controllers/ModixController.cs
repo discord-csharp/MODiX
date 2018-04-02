df5590ca19939806579bef57bc09d9cb1e1afdd8
John Mazouri <jmazouri@gmail.com>
2018-04-02 11:17:10 -0400

    These partial stagings i'm doing will be the death of me

--- Modix.WebServer/Controllers/ModixController.cs
+++ Modix.WebServer/Controllers/ModixController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Modix.WebServer.Models;
