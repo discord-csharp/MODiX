ca20db9a1ff6477f4a39e990d3ef82b70c20f1a7
Cisien <ccurwick@exoronet.net>
2018-04-04 19:06:02 -0700

    about that space...

--- before-deploy.ps1
+++ before-deploy.ps1
@@ -13,5 +13,5 @@ if (Enter-OncePerDeployment "install_docker_image")
 	docker stop Modix
 	docker rm Modix
 	docker pull "cisien/modix:$tag"
-	docker run -d -p 8080:8080 -e "SentryToken=$env:SentryToken" -e "Token=$env:ModixToken" -e "ReplToken=$env:ReplServiceToken" -e "StackoverflowToken=$env:SOToken" -e "MODIX_DB_CONNECTION=$env:ConnectionString" -e "log_webhook_id=$env:log_webhook_id" -e "log_webhook_token=$env:log_webhook_token" -e "DiscordClientId=$env:DiscordClientId" -e "DiscordClientSecret=$env:DiscordClientSecret" -e "ASPNETCORE_URLS=$env:ASPNETCORE_URLS" --name=Modix -v modix:c:\app\config --restart=always --link=CSDiscord --net=nat --net-alias=Modix --hostname=Modix--isolation=hyperv "cisien/modix:$tag"
+	docker run -d -p 8080:8080 -e "SentryToken=$env:SentryToken" -e "Token=$env:ModixToken" -e "ReplToken=$env:ReplServiceToken" -e "StackoverflowToken=$env:SOToken" -e "MODIX_DB_CONNECTION=$env:ConnectionString" -e "log_webhook_id=$env:log_webhook_id" -e "log_webhook_token=$env:log_webhook_token" -e "DiscordClientId=$env:DiscordClientId" -e "DiscordClientSecret=$env:DiscordClientSecret" -e "ASPNETCORE_URLS=$env:ASPNETCORE_URLS" --name=Modix -v modix:c:\app\config --restart=always --link=CSDiscord --net=nat --net-alias=Modix --hostname=Modix --isolation=hyperv "cisien/modix:$tag"
 }
