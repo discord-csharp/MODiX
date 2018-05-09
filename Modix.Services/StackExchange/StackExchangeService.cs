54b0d9026c055e9397d5c11c8bb2fb369d687fe8
Mario Cacciatore <mcacciatore@edatadimensions.com>
2018-05-09 11:20:00 -0400

    Remove unused namespaces

--- Modix.Services/StackExchange/StackExchangeService.cs
+++ Modix.Services/StackExchange/StackExchangeService.cs
@@ -1,10 +1,7 @@
 ﻿using Newtonsoft.Json;
 using System;
-using System.IO;
-using System.IO.Compression;
 using System.Net;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Modix.Services.StackExchange
