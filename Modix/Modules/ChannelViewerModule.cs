ffe5d5f30fc1a5d9a24f9b054450f18cba2abdbb
Matt Jones <matthewj866@gmail.com>
2018-07-08 19:51:50 +0100

    commit before upstream merge.

---
+++ Modix/Modules/ChannelViewerModule.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace Modix.Modules
+{
+    [Name("Channel Viewer"), Summary("Allows you to show and hide specific channels.")]
+    public class ChannelViewerModule : ModuleBase
+    {
+        public async Task HideChannelAsync(IGuildChannel channel)
+        {
+            await channel.AddPermissionOverwriteAsync(Context.User, OverwritePermissions.DenyAll(channel));
+            await ReplyAsync("Channel hidden.");
+        }
+
+        public async Task ShowChannelAsync(IGuildChannel channel)
+        {
+            await channel.AddPermissionOverwriteAsync(Context.User, OverwritePermissions.AllowAll(channel));
+            await ReplyAsync("Channel hidden.");
+        }
+    }
+}
