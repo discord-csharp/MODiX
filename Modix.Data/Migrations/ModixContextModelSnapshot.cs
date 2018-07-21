a800fe6e7b396a24fd931ee04788540bce6fe530
Jasonhughes94 <jason.hughes@arnoldclark.com>
2018-07-21 17:31:46 +0100

    Resolve Merge Conflict files not being removed after merge. Update Migration script to be in line with master

--- Modix.Data/Migrations/ModixContextModelSnapshot.cs
+++ Modix.Data/Migrations/ModixContextModelSnapshot.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using Modix.Data;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace Modix.Data.Migrations
@@ -19,299 +20,303 @@ namespace Modix.Data.Migrations
                 .HasAnnotation("Relational:MaxIdentifierLength", 63);
 
             modelBuilder.Entity("Modix.Data.Models.BehaviourConfiguration", b =>
-                {
-                    b.Property<int>("Id")
-                        .ValueGeneratedOnAdd();
+            {
+                b.Property<int>("Id")
+                    .ValueGeneratedOnAdd();
 
-                    b.Property<string>("Category")
-                        .IsRequired();
+                b.Property<string>("Category")
+                    .IsRequired();
 
-                    b.Property<string>("Key")
-                        .IsRequired();
+                b.Property<string>("Key")
+                    .IsRequired();
 
-                    b.Property<string>("Value")
-                        .IsRequired();
+                b.Property<string>("Value")
+                    .IsRequired();
 
-                    b.HasKey("Id");
+                b.HasKey("Id");
 
-                    b.ToTable("BehaviourConfigurations");
-                });
+                b.ToTable("BehaviourConfigurations");
+            });
 
             modelBuilder.Entity("Modix.Data.Models.Core.ClaimMappingEntity", b =>
-                {
-                    b.Property<long>("Id")
-                        .ValueGeneratedOnAdd();
+            {
+                b.Property<long>("Id")
+                    .ValueGeneratedOnAdd();
 
-                    b.Property<string>("Claim")
-                        .IsRequired();
+                b.Property<string>("Claim")
+                    .IsRequired();
 
-                    b.Property<long>("CreateActionId");
+                b.Property<long>("CreateActionId");
 
-                    b.Property<long>("GuildId");
+                b.Property<long>("GuildId");
 
-                    b.Property<long?>("RescindActionId");
+                b.Property<long?>("RescindActionId");
 
-                    b.Property<long?>("RoleId");
+                b.Property<long?>("RoleId");
 
-                    b.Property<string>("Type")
-                        .IsRequired();
+                b.Property<string>("Type")
+                    .IsRequired();
 
-                    b.Property<long?>("UserId");
+                b.Property<long?>("UserId");
 
-                    b.HasKey("Id");
+                b.HasKey("Id");
 
-                    b.HasIndex("CreateActionId")
-                        .IsUnique();
+                b.HasIndex("CreateActionId")
+                    .IsUnique();
 
-                    b.HasIndex("RescindActionId")
-                        .IsUnique();
+                b.HasIndex("RescindActionId")
+                    .IsUnique();
 
-                    b.ToTable("ClaimMappings");
-                });
+                b.ToTable("ClaimMappings");
+            });
 
             modelBuilder.Entity("Modix.Data.Models.Core.ConfigurationActionEntity", b =>
-                {
-                    b.Property<long>("Id")
-                        .ValueGeneratedOnAdd();
+            {
+                b.Property<long>("Id")
+                    .ValueGeneratedOnAdd();
 
-                    b.Property<long?>("ClaimMappingId");
+                b.Property<long?>("ClaimMappingId");
 
-                    b.Property<DateTimeOffset>("Created");
+                b.Property<DateTimeOffset>("Created");
 
-                    b.Property<long>("CreatedById");
+                b.Property<long>("CreatedById");
 
-                    b.Property<string>("Type")
-                        .IsRequired();
+                b.Property<string>("Type")
+                    .IsRequired();
 
-                    b.HasKey("Id");
+                b.HasKey("Id");
 
-                    b.HasIndex("ClaimMappingId");
+                b.HasIndex("ClaimMappingId");
 
-                    b.HasIndex("CreatedById");
+                b.HasIndex("CreatedById");
 
-                    b.ToTable("ConfigurationActions");
-                });
+                b.ToTable("ConfigurationActions");
+            });
 
             modelBuilder.Entity("Modix.Data.Models.Core.UserEntity", b =>
-                {
-                    b.Property<long>("Id")
-                        .ValueGeneratedOnAdd();
+            {
+                b.Property<long>("Id")
+                    .ValueGeneratedOnAdd();
 
-                    b.Property<string>("Discriminator")
-                        .IsRequired();
+                b.Property<string>("Discriminator")
+                    .IsRequired();
 
-                    b.Property<DateTimeOffset>("FirstSeen");
+                b.Property<DateTimeOffset>("FirstSeen");
 
-                    b.Property<DateTimeOffset>("LastSeen");
+                b.Property<DateTimeOffset>("LastSeen");
 
-                    b.Property<string>("Nickname");
+                b.Property<string>("Nickname");
 
-                    b.Property<string>("Username")
-                        .IsRequired();
+                b.Property<string>("Username")
+                    .IsRequired();
 
-                    b.HasKey("Id");
+                b.HasKey("Id");
 
-                    b.ToTable("Users");
-                });
+                b.ToTable("Users");
+            });
 
             modelBuilder.Entity("Modix.Data.Models.Moderation.InfractionEntity", b =>
-                {
-                    b.Property<long>("Id")
-                        .ValueGeneratedOnAdd();
+            {
+                b.Property<long>("Id")
+                    .ValueGeneratedOnAdd();
 
-                    b.Property<long>("CreateActionId");
+                b.Property<long>("CreateActionId");
 
-                    b.Property<TimeSpan?>("Duration");
+                b.Property<DateTimeOffset>("Created");
+                b.Property<TimeSpan?>("Duration");
 
                     b.Property<long>("GuildId");
 
                     b.Property<string>("Reason")
                         .IsRequired();
 
-                    b.Property<long?>("RescindActionId");
+                b.Property<long?>("RescindActionId");
 
-                    b.Property<long>("SubjectId");
+                b.Property<long>("SubjectId");
 
-                    b.Property<string>("Type")
-                        .IsRequired();
+                b.Property<string>("Type")
+                    .IsRequired();
 
-                    b.HasKey("Id");
+                b.HasKey("Id");
 
-                    b.HasIndex("CreateActionId")
-                        .IsUnique();
+                b.HasIndex("CreateActionId")
+                    .IsUnique();
 
-                    b.HasIndex("RescindActionId")
-                        .IsUnique();
+                b.HasIndex("RescindActionId")
+                    .IsUnique();
 
-                    b.HasIndex("SubjectId");
+                b.HasIndex("SubjectId");
 
-                    b.ToTable("Infractions");
-                });
+                b.ToTable("Infractions");
+            });
 
             modelBuilder.Entity("Modix.Data.Models.Moderation.ModerationActionEntity", b =>
-                {
-                    b.Property<long>("Id")
-                        .ValueGeneratedOnAdd();
+            {
+                b.Property<long>("Id")
+                    .ValueGeneratedOnAdd();
 
-                    b.Property<DateTimeOffset>("Created");
+                b.Property<DateTimeOffset>("Created");
 
-                    b.Property<long>("CreatedById");
+                b.Property<long>("CreatedById");
 
-                    b.Property<long?>("InfractionId");
+                b.Property<long?>("InfractionId");
 
-                    b.Property<string>("Type")
-                        .IsRequired();
+                b.Property<string>("Reason")
+                    .IsRequired();
+                b.Property<string>("Type")
+                    .IsRequired();
 
-                    b.HasKey("Id");
+                b.HasKey("Id");
 
-                    b.HasIndex("CreatedById");
+                b.HasIndex("CreatedById");
 
-                    b.HasIndex("InfractionId");
+                b.HasIndex("InfractionId");
 
-                    b.ToTable("ModerationActions");
-                });
+                b.ToTable("ModerationActions");
+            });
 
             modelBuilder.Entity("Modix.Data.Models.Moderation.ModerationConfigEntity", b =>
-                {
-                    b.Property<long>("GuildId")
-                        .ValueGeneratedOnAdd();
+            {
+                b.Property<long>("GuildId")
+                    .ValueGeneratedOnAdd();
 
-                    b.Property<DateTimeOffset>("Created");
+                b.Property<DateTimeOffset>("Created");
 
-                    b.Property<long>("MuteRoleId");
+                b.Property<long>("MuteRoleId");
 
-                    b.HasKey("GuildId");
+                b.HasKey("GuildId");
 
-                    b.ToTable("ModerationConfigs");
-                });
+                b.ToTable("ModerationConfigs");
+            });
+
+            modelBuilder.Entity("Modix.Data.Models.Promotion.PromotionCampaignEntity", b =>
+            {
+                b.Property<long>("PromotionCampaignId")
+                    .ValueGeneratedOnAdd();
+
+                b.Property<long>("PromotionForId");
+
+                b.Property<DateTimeOffset>("StartDate");
+
+                b.Property<int>("Status");
+
+                b.HasKey("PromotionCampaignId");
+
+                b.HasIndex("PromotionForId");
+
+                b.ToTable("PromotionCampaigns");
+            });
+
+            modelBuilder.Entity("Modix.Data.Models.Promotion.PromotionCommentEntity", b =>
+            {
+                b.Property<long>("PromotionCommentId")
+                    .ValueGeneratedOnAdd();
+
+                b.Property<string>("Body");
+
+                b.Property<DateTimeOffset>("PostedDate");
+
+                b.Property<long?>("PromotionCampaignId");
+
+                b.Property<int>("Sentiment");
+
+                b.HasKey("PromotionCommentId");
+
+                b.HasIndex("PromotionCampaignId");
+
+                b.ToTable("PromotionComments");
+            });
 
             modelBuilder.Entity("Modix.Data.Models.Moderation.NoteEntity", b =>
-                {
-                    b.Property<int>("Id")
-                        .ValueGeneratedOnAdd();
+            {
+                b.Property<int>("Id")
+                    .ValueGeneratedOnAdd();
 
-                    b.Property<string>("Username");
+                b.Property<string>("Username");
 
-                    b.Property<string>("Message");
+                b.Property<string>("Message");
 
-                    b.Property<string>("RecordedBy");
+                b.Property<string>("RecordedBy");
 
-                    b.Property<DateTime>("RecordedTime");
+                b.Property<DateTime>("RecordedTime");
 
-                    b.Property<decimal>("UserId")
-                        .HasConversion(new ValueConverter<decimal, decimal>(v => default(decimal), v => default(decimal), new ConverterMappingHints(precision: 20, scale: 0)));
+                b.Property<decimal>("UserId")
+                    .HasConversion(new ValueConverter<decimal, decimal>(v => default(decimal), v => default(decimal), new ConverterMappingHints(precision: 20, scale: 0)));
 
-                    b.HasKey("Id");
+                b.HasKey("Id");
 
-                    b.ToTable("Notes");
-                });
-
-            modelBuilder.Entity("Modix.Data.Models.Promotion.PromotionCampaignEntity", b =>
-                {
-                    b.Property<long>("PromotionCampaignId")
-                        .ValueGeneratedOnAdd();
-
-                    b.Property<long>("PromotionForId");
-
-                    b.Property<DateTimeOffset>("StartDate");
-
-                    b.Property<int>("Status");
-
-                    b.HasKey("PromotionCampaignId");
-
-                    b.HasIndex("PromotionForId");
-
-                    b.ToTable("PromotionCampaigns");
-                });
-
-            modelBuilder.Entity("Modix.Data.Models.Promotion.PromotionCommentEntity", b =>
-                {
-                    b.Property<long>("PromotionCommentId")
-                        .ValueGeneratedOnAdd();
-
-                    b.Property<string>("Body");
-
-                    b.Property<DateTimeOffset>("PostedDate");
-
-                    b.Property<long?>("PromotionCampaignId");
-
-                    b.Property<int>("Sentiment");
-
-                    b.HasKey("PromotionCommentId");
-
-                    b.HasIndex("PromotionCampaignId");
-
-                    b.ToTable("PromotionComments");
-                });
+                b.ToTable("Notes");
+            });
 
             modelBuilder.Entity("Modix.Data.Models.Core.ClaimMappingEntity", b =>
-                {
-                    b.HasOne("Modix.Data.Models.Core.ConfigurationActionEntity", "CreateAction")
-                        .WithOne("CreatedClaimMapping")
-                        .HasForeignKey("Modix.Data.Models.Core.ClaimMappingEntity", "CreateActionId")
-                        .OnDelete(DeleteBehavior.Cascade);
+            {
+                b.HasOne("Modix.Data.Models.Core.ConfigurationActionEntity", "CreateAction")
+                    .WithOne("CreatedClaimMapping")
+                    .HasForeignKey("Modix.Data.Models.Core.ClaimMappingEntity", "CreateActionId")
+                    .OnDelete(DeleteBehavior.Cascade);
 
-                    b.HasOne("Modix.Data.Models.Core.ConfigurationActionEntity", "RescindAction")
-                        .WithOne("RescindedClaimMapping")
-                        .HasForeignKey("Modix.Data.Models.Core.ClaimMappingEntity", "RescindActionId");
-                });
+                b.HasOne("Modix.Data.Models.Core.ConfigurationActionEntity", "RescindAction")
+                    .WithOne("RescindedClaimMapping")
+                    .HasForeignKey("Modix.Data.Models.Core.ClaimMappingEntity", "RescindActionId");
+            });
 
             modelBuilder.Entity("Modix.Data.Models.Core.ConfigurationActionEntity", b =>
-                {
-                    b.HasOne("Modix.Data.Models.Core.ClaimMappingEntity", "ClaimMapping")
-                        .WithMany()
-                        .HasForeignKey("ClaimMappingId");
+            {
+                b.HasOne("Modix.Data.Models.Core.ClaimMappingEntity", "ClaimMapping")
+                    .WithMany()
+                    .HasForeignKey("ClaimMappingId");
 
-                    b.HasOne("Modix.Data.Models.Core.UserEntity", "CreatedBy")
-                        .WithMany()
-                        .HasForeignKey("CreatedById")
-                        .OnDelete(DeleteBehavior.Cascade);
-                });
+                b.HasOne("Modix.Data.Models.Core.UserEntity", "CreatedBy")
+                    .WithMany()
+                    .HasForeignKey("CreatedById")
+                    .OnDelete(DeleteBehavior.Cascade);
+            });
 
             modelBuilder.Entity("Modix.Data.Models.Moderation.InfractionEntity", b =>
-                {
-                    b.HasOne("Modix.Data.Models.Moderation.ModerationActionEntity", "CreateAction")
-                        .WithOne("CreatedInfraction")
-                        .HasForeignKey("Modix.Data.Models.Moderation.InfractionEntity", "CreateActionId")
-                        .OnDelete(DeleteBehavior.Cascade);
+            {
+                b.HasOne("Modix.Data.Models.Moderation.ModerationActionEntity", "CreateAction")
+                    .WithOne("CreatedInfraction")
+                    .HasForeignKey("Modix.Data.Models.Moderation.InfractionEntity", "CreateActionId")
+                    .OnDelete(DeleteBehavior.Cascade);
 
-                    b.HasOne("Modix.Data.Models.Moderation.ModerationActionEntity", "RescindAction")
-                        .WithOne("RescindedInfraction")
-                        .HasForeignKey("Modix.Data.Models.Moderation.InfractionEntity", "RescindActionId");
+                b.HasOne("Modix.Data.Models.Moderation.ModerationActionEntity", "RescindAction")
+                    .WithOne("RescindedInfraction")
+                    .HasForeignKey("Modix.Data.Models.Moderation.InfractionEntity", "RescindActionId");
 
-                    b.HasOne("Modix.Data.Models.Core.UserEntity", "Subject")
-                        .WithMany()
-                        .HasForeignKey("SubjectId")
-                        .OnDelete(DeleteBehavior.Cascade);
-                });
+                b.HasOne("Modix.Data.Models.Core.UserEntity", "Subject")
+                    .WithMany()
+                    .HasForeignKey("SubjectId")
+                    .OnDelete(DeleteBehavior.Cascade);
+            });
 
             modelBuilder.Entity("Modix.Data.Models.Moderation.ModerationActionEntity", b =>
-                {
-                    b.HasOne("Modix.Data.Models.Core.UserEntity", "CreatedBy")
-                        .WithMany()
-                        .HasForeignKey("CreatedById")
-                        .OnDelete(DeleteBehavior.Cascade);
+            {
+                b.HasOne("Modix.Data.Models.Core.UserEntity", "CreatedBy")
+                    .WithMany()
+                    .HasForeignKey("CreatedById")
+                    .OnDelete(DeleteBehavior.Cascade);
 
-                    b.HasOne("Modix.Data.Models.Moderation.InfractionEntity", "Infraction")
-                        .WithMany()
-                        .HasForeignKey("InfractionId");
-                });
+                b.HasOne("Modix.Data.Models.Moderation.InfractionEntity", "Infraction")
+                    .WithMany()
+                    .HasForeignKey("InfractionId");
+            });
 
             modelBuilder.Entity("Modix.Data.Models.Promotion.PromotionCampaignEntity", b =>
-                {
-                    b.HasOne("Modix.Data.Models.Core.UserEntity", "PromotionFor")
-                        .WithMany()
-                        .HasForeignKey("PromotionForId")
-                        .OnDelete(DeleteBehavior.Cascade);
-                });
+            {
+                b.HasOne("Modix.Data.Models.Core.UserEntity", "PromotionFor")
+                    .WithMany()
+                    .HasForeignKey("PromotionForId")
+                    .OnDelete(DeleteBehavior.Cascade);
+            });
 
             modelBuilder.Entity("Modix.Data.Models.Promotion.PromotionCommentEntity", b =>
-                {
-                    b.HasOne("Modix.Data.Models.Promotion.PromotionCampaignEntity", "PromotionCampaign")
-                        .WithMany("Comments")
-                        .HasForeignKey("PromotionCampaignId");
-                });
+            {
+                b.HasOne("Modix.Data.Models.Promotion.PromotionCampaignEntity", "PromotionCampaign")
+                    .WithMany("Comments")
+                    .HasForeignKey("PromotionCampaignId");
+            });
 #pragma warning restore 612, 618
         }
     }
 }
+
--- Modix.Data/ModixContext.cs
+++ Modix.Data/ModixContext.cs
@@ -6,6 +6,7 @@ using Modix.Data.Models.Promotion;
 
 namespace Modix.Data
 {
+
     public class ModixContext : DbContext
     {
         public ModixContext(DbContextOptions<ModixContext> options) : base(options)
@@ -18,21 +19,13 @@ namespace Modix.Data
         }
 
         public DbSet<ConfigurationActionEntity> ConfigurationActions { get; set; }
-
         public DbSet<BehaviourConfiguration> BehaviourConfigurations { get; set; }
-
         public DbSet<UserEntity> Users { get; set; }
-
         public DbSet<ClaimMappingEntity> ClaimMappings { get; set; }
-
         public DbSet<ModerationConfigEntity> ModerationConfigs { get; set; }
-
         public DbSet<ModerationActionEntity> ModerationActions { get; set; }
-
         public DbSet<InfractionEntity> Infractions { get; set; }
-
         public DbSet<PromotionCampaignEntity> PromotionCampaigns { get; set; }
-
         public DbSet<PromotionCommentEntity> PromotionComments { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
