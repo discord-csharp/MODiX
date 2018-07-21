dad10df392d02df913e5028395d79392fa23ebe1
Jasonhughes94 <jason.hughes@arnoldclark.com>
2018-07-21 12:33:30 +0100

    Add Note Services

---
+++ Modix.Services/Notes/INoteRemoverService.cs
@@ -0,0 +1,10 @@
+﻿namespace Modix.Services.Notes
+{
+    using System.Threading.Tasks;
+    using Data.Models.Moderation;
+
+    public interface INoteRemoverService
+    {
+        Task<bool> Remove(NoteEntity note);
+    }
+}
---
+++ Modix.Services/Notes/INoteRetrieverService.cs
@@ -0,0 +1,11 @@
+﻿namespace Modix.Services.Notes
+{
+    using System.Collections.Generic;
+    using Data.Models.Moderation;
+
+    public interface INoteRetrieverService
+    {
+        List<NoteEntity> FindNotesForUser(ulong userId);
+        NoteEntity FindNoteById(int noteId);
+    }
+}
