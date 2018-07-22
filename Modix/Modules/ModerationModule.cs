e30716112951619117c3c272e70094e5fbf8d2e3
JakenVeina <jakenveina@yahoo.com>
2018-07-21 21:11:23 -0500

    Added skeleton for ModerationModule.

---
+++ Modix/Modules/ModerationModule.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Discord;
+using Discord.Commands;
+
+using Modix.Data.Models.Moderation;
+using Modix.Services.Moderation;
+
+namespace Modix.Modules
+{
+    [Name("Moderation")]
+    [Summary("Guild moderation commands")]
+    public class ModerationModule : ModuleBase
+    {
+        public ModerationModule(IModerationService moderationService)
+        {
+            ModerationService = moderationService ?? throw new ArgumentNullException(nameof(moderationService));
+        }
+
+        public Task Warn()
+            => throw new NotImplementedException();
+
+        public Task RescindWarn()
+            => throw new NotImplementedException();
+
+        public Task Mute()
+            => throw new NotImplementedException();
+
+        public Task TempMute()
+            => throw new NotImplementedException();
+
+        public Task UnMute()
+            => throw new NotImplementedException();
+
+        public Task Ban()
+            => throw new NotImplementedException();
+
+        public Task UnBan()
+            => throw new NotImplementedException();
+
+        public Task Search()
+            => throw new NotImplementedException();
+
+        internal protected IModerationService ModerationService { get; }
+    }
+}
