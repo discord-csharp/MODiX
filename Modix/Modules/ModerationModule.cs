084ed9f086bbf3dbd23189f0f9af353fd2505cce
JakenVeina <jakenveina@yahoo.com>
2018-07-22 00:05:29 -0500

    Implemented (most of) ModerationModule. The rest will need a few core changes to the infractions system.

--- Modix/Modules/ModerationModule.cs
+++ Modix/Modules/ModerationModule.cs
@@ -18,28 +18,60 @@ namespace Modix.Modules
             ModerationService = moderationService ?? throw new ArgumentNullException(nameof(moderationService));
         }
 
-        public Task Warn()
+        [Command("warn")]
+        [Summary("Issue a warning to a user.")]
+        public Task Warn(
+            [Summary("The user to which the warning is being issued.")]
+                IGuildUser subject,
+            [Summary("The reason for the warning.")]
+            [Remainder]
+                string reason)
+            => ModerationService.CreateInfractionAsync(InfractionType.Warning, subject.Id, reason, null);
+
+        [Command("mute")]
+        [Summary("Mute a user.")]
+        public Task Mute(
+            [Summary("The user to be muted.")]
+                IGuildUser subject,
+            [Summary("The reason for the mute.")]
+            [Remainder]
+                string reason)
+            => ModerationService.CreateInfractionAsync(InfractionType.Mute, subject.Id, reason, null);
+
+        [Command("mute")]
+        [Summary("Mute a user, for a temporary amount of time.")]
+        public Task TempMute(
+            [Summary("The user to be muted.")]
+                IGuildUser subject,
+            [Summary("The duration of the mute.")]
+                TimeSpan duration,
+            [Summary("The reason for the mute.")]
+            [Remainder]
+                string reason)
+            => ModerationService.CreateInfractionAsync(InfractionType.Mute, subject.Id, reason, duration);
+
+        [Command("unmute")]
+        [Summary("Remove a mute that has been applied to a user.")]
+        public Task UnMute(
+            [Summary("The user to be un-muted.")]
+                IGuildUser subject)
             => throw new NotImplementedException();
 
-        public Task RescindWarn()
-            => throw new NotImplementedException();
+        [Command("ban")]
+        [Summary("Ban a user from the current guild.")]
+        public Task Ban(
+            [Summary("The user to be banned.")]
+                IGuildUser subject,
+            [Summary("The reason for the ban.")]
+            [Remainder]
+                string reason)
+            => ModerationService.CreateInfractionAsync(InfractionType.Ban, subject.Id, reason, null);
 
-        public Task Mute()
-            => throw new NotImplementedException();
-
-        public Task TempMute()
-            => throw new NotImplementedException();
-
-        public Task UnMute()
-            => throw new NotImplementedException();
-
-        public Task Ban()
-            => throw new NotImplementedException();
-
-        public Task UnBan()
-            => throw new NotImplementedException();
-
-        public Task Search()
+        [Command("unban")]
+        [Summary("Remove a ban that has been applied to a user.")]
+        public Task UnBan(
+            [Summary("The user to be un-banned.")]
+                IGuildUser subject)
             => throw new NotImplementedException();
 
         internal protected IModerationService ModerationService { get; }
