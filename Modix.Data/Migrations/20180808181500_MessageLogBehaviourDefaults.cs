89aa4260626855e637c2b4e7e01918e75ea1cd4b
John Mazouri <jmazouri@gmail.com>
2018-08-08 14:19:41 -0400

    Added migration to insert config for message log

---
+++ Modix.Data/Migrations/20180808181500_MessageLogBehaviourDefaults.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Modix.Data.Models;
+
+namespace Modix.Data.Migrations
+{
+    public partial class MessageLogBehaviourDefaults : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData("BehaviourConfigurations", new[] { "Category", "Key", "Value" }, new[] { BehaviourCategory.MessageLogging.ToString(), "LoggingChannelId", "380603419410432002" });
+            migrationBuilder.InsertData("BehaviourConfigurations", new[] { "Category", "Key", "Value" }, new[] { BehaviourCategory.MessageLogging.ToString(), "OldMessageAgeLimit", "7" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
