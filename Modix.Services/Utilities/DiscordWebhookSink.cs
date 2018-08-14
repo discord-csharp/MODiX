c4822118e279b35be9e987e9c683e84fead572a9
Cisien <ccurwick@exoronet.net>
2018-08-13 18:32:08 -0700

    upload stack trace to paste.mod.gg and put the link in the embed

--- Modix.Services/Utilities/DiscordWebhookSink.cs
+++ Modix.Services/Utilities/DiscordWebhookSink.cs
@@ -5,6 +5,8 @@ using Serilog.Events;
 using Serilog.Configuration;
 using Discord.Webhook;
 using Discord;
+using Modix.Services.AutoCodePaste;
+using Newtonsoft.Json;
 
 namespace Modix.Services.Utilities
 {
@@ -13,12 +15,18 @@ namespace Modix.Services.Utilities
         private readonly ulong _webhookId;
         private readonly string _webhookToken;
         private readonly IFormatProvider _formatProvider;
-
+        private readonly JsonSerializerSettings _jsonSerializerSettings;
         public DiscordWebhookSink(ulong webhookId, string webhookToken, IFormatProvider formatProvider)
         {
             _webhookId = webhookId;
             _webhookToken = webhookToken;
             _formatProvider = formatProvider;
+
+            _jsonSerializerSettings = new JsonSerializerSettings
+            {
+                Formatting = Formatting.Indented,
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+            };
         }
         public void Emit(LogEvent logEvent)
         {
@@ -33,8 +41,29 @@ namespace Modix.Services.Utilities
                 .AddField(new EmbedFieldBuilder()
                     .WithIsInline(false)
                     .WithName($"LogLevel: {logEvent.Level}")
-                    .WithValue(Format.Code($"{formattedMessage}\n{logEvent.Exception?.ToString()}".TruncateTo(1010))));
+                    .WithValue(Format.Code($"{formattedMessage}\n{logEvent.Exception?.Message}")));
+            try
+            {
+                var pasteHandler = new CodePasteService();
+                var url = pasteHandler.UploadCode(JsonConvert.SerializeObject(logEvent, _jsonSerializerSettings), "json").GetAwaiter().GetResult();
 
+                message.AddField(new EmbedFieldBuilder()
+                    .WithIsInline(false)
+                    .WithName("Full Exception")
+                    .WithValue($"[view on paste.mod.gg]({url})"));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Unable to upload stacktrace.{ex}");
+                message.AddField(new EmbedFieldBuilder()
+                    .WithIsInline(false)
+                    .WithName("Stack trace")
+                    .WithValue(Format.Code($"{formattedMessage}\n{logEvent.Exception?.ToString().TruncateTo(1000)}")));
+                message.AddField(new EmbedFieldBuilder()
+                    .WithIsInline(false)
+                    .WithName("Upload Failure Exception")
+                    .WithValue(Format.Code($"{ex.ToString().TruncateTo(1000)}")));
+            }
             webhookClient.SendMessageAsync(string.Empty, embeds: new[] { message.Build() }, username: "Modix Logger");
         }
     }
@@ -50,7 +79,7 @@ namespace Modix.Services.Utilities
     {
         public static string TruncateTo(this string str, int length)
         {
-            if(str.Length < length)
+            if (str.Length < length)
             {
                 return str;
             }
