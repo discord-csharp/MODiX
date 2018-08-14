8871c83dc9021d8433071089dc05434f549ba532
JakenVeina <jakenveina@yahoo.com>
2018-08-13 21:56:43 -0500

    Same as 1c9beed, for new code.

--- Modix.Services/Core/ChannelService.cs
+++ Modix.Services/Core/ChannelService.cs
@@ -7,6 +7,19 @@ using Modix.Data.Repositories;
 
 namespace Modix.Services.Core
 {
+    /// <summary>
+    /// Provides methods for managing and interacting with Discord channels, within the application.
+    /// </summary>
+    public interface IChannelService
+    {
+        /// <summary>
+        /// Updates information about the given channel within the channel tracking..
+        /// </summary>
+        /// <param name="channel">The channel whose info is to be tracked.</param>
+        /// <returns>A <see cref="Task"/> that will complete when the operation has completed.</returns>
+        Task TrackChannelAsync(IGuildChannel channel);
+    }
+
     /// <inheritdoc />
     public class ChannelService : IChannelService
     {
