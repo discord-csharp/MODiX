ae1eb52c46fbed4b4b33b45751276ef98494a585
Ainz <redmon.chase@gmail.com>
2018-09-21 02:29:37 -0400

    Discord Embed for Displaying infractions

--- Modix/Modules/InfractionModule.cs
+++ Modix/Modules/InfractionModule.cs
@@ -76,6 +76,93 @@ namespace Modix.Modules
             if(replyBuilder.Length > 0)
                 await ReplyAsync(Format.Code(replyBuilder.ToString()));
         }
+        
+        [Command("search embed")]
+        [Summary("Display all infractions for a user, that haven't been deleted.")]
+        public async Task SearchEmbed(
+            [Summary("The user whose infractions are to be displayed.")]
+            IGuildUser subject)
+        {
+            var infractions = await ModerationService.SearchInfractionsAsync(
+                new InfractionSearchCriteria
+                {
+                    GuildId = subject.GuildId,
+                    SubjectId = subject.Id,
+                    IsDeleted = false
+                },
+                new[]
+                {
+                    new SortingCriteria { PropertyName = "CreateAction.Created", Direction = SortDirection.Descending }
+                });
+
+            if(infractions.Count == 0)
+            {
+                await ReplyAsync(Format.Code("No infractions"));
+                return;
+            }
+
+            var infractionQuery = infractions.Select(infraction => new
+            {
+                Id = infraction.Id,
+                Created = infraction.CreateAction.Created.ToUniversalTime().ToString("yyyy MMM dd HH:mm"),
+                Type = infraction.Type.ToString(),
+                Subject = infraction.Subject.Username,
+                Creator = infraction.CreateAction.CreatedBy.DisplayName,
+                State = (infraction.RescindAction != null) ? "Rescinded"
+                    : (infraction.Expires != null) ? "Will Expire"
+                    : "Active",
+                Reason = infraction.Reason
+            }).OrderBy(s => s.Type);
+
+            var builder = new EmbedBuilder()
+                .WithTitle($"Infractions for user {subject.Nickname}")
+                //.WithDescription("This user has a total of 5 mutes, 7 warnings, and 3 notices.")
+                .WithUrl("https://discordapp.com")
+                .WithColor(new Color(0xA3BF0B))
+                //.WithTimestamp(DateTimeOffset.FromUnixTimeMilliseconds(1537508496766))
+                .WithFooter(footer =>
+                {
+                    footer
+                        .WithText("Infractions")
+                        .WithIconUrl("https://cdn.discordapp.com/embed/avatars/0.png");
+                })
+                .WithThumbnailUrl("https://cdn.discordapp.com/embed/avatars/0.png");
+
+
+            foreach (var infraction in infractionQuery)
+            {
+                builder.AddField(infraction.Type, $"ID: {infraction.Id} - Reason: {infraction.Reason}");
+            }
+
+            /*var infractionQuery = from infraction in infractions
+                group infraction by infraction.Type
+                into groupedInfractions
+                orderby groupedInfractions.Key
+                select groupedInfractions;
+
+            var index = 0;*/
+            
+            // good for grouping infractions but doesn't work because discord embeds strong arm you into using one value per field.
+            // hacky workaround. Probably not worth it
+            /*foreach (var infractionGroup in infractionQuery)
+            {
+                
+                
+                foreach (var infraction in infractionGroup)
+                {
+                    builder.Fields[index].Value = $"ID: {infraction.Id} - Reason: {infraction.Reason}";
+                }
+                
+                index += 1;
+            }*/
+
+            var embed = builder.Build();
+            
+            await Context.Channel.SendMessageAsync(
+                    "Requested by @username",
+                    embed: embed)
+                .ConfigureAwait(false);
+        }
 
         [Command("delete")]
         [Summary("Marks an infraction as deleted, so it no longer appears within infraction search results")]
