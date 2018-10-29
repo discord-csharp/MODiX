1dca0e7b2b5accee02db0ca7ddb3703d7be68b0e
Jasonhughes94 <jason.hughes@arnoldclark.com>
2018-10-29 21:45:38 +0000

    Remove unused using statements - missed a few from Modix proj

--- Modix/Modules/FunModule.cs
+++ Modix/Modules/FunModule.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
-using Discord.WebSocket;
 using Serilog;
 
 namespace Modix.Modules
--- Modix/Modules/MarkerRoleModule.cs
+++ Modix/Modules/MarkerRoleModule.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
-using Microsoft.AspNetCore.Mvc.TagHelpers;
 
 namespace Modix.Modules
 {
