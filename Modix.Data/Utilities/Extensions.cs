8770984f65ad431fcc8fb26f7dde7260f68117c3
Jasonhughes94 <jason.hughes@arnoldclark.com>
2018-10-29 22:10:00 +0000

    Update Extensions - Remove unused extensions and clean up Truncate
    Removed redundant else and use ternary for the return. Overall I think this just looks a lot cleaner

--- Modix.Data/Utilities/Extensions.cs
+++ Modix.Data/Utilities/Extensions.cs
@@ -5,22 +5,6 @@ namespace Modix.Data.Utilities
 {
     public static class Extensions
     {
-        public static long ToLong(this ulong number)
-        {
-            if (!long.TryParse((number.ToString()), out var convertedNumber)) 
-                throw new AggregateException("Could not convert ulong to long");
-
-            return convertedNumber;
-        }
-
-        public static ulong ToUlong(this long number)
-        {
-            if (!ulong.TryParse((number.ToString()), out var convertedNumber))
-                throw new AggregateException("Could not convert long to ulong");
-
-            return convertedNumber;
-        }
-
         public static string Truncate(this string value, int maxLength, int maxLines, string suffix = "…")
         {
             if (string.IsNullOrEmpty(value)) return value;
@@ -29,18 +13,10 @@ namespace Modix.Data.Utilities
             {
                 return value;
             }
-            else
-            {
-                var lines = value.Split("\n".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
-                
-                if (lines.Length > maxLines)
-                {
-                    //merge everything back with newlines
-                    return String.Join("\n", lines.Take(maxLines));
-                }
 
-                return $"{value.Substring(0, maxLength).Trim()}{suffix}";
-            }
+            var lines = value.Split("\n".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+                
+            return lines.Length > maxLines ? string.Join("\n", lines.Take(maxLines)) : $"{value.Substring(0, maxLength).Trim()}{suffix}";
         }
 
         public static bool OrdinalContains(this string value, string search)
