6f6a94fed7f18d7679c19c577805dd6c14437770
JakenVeina <jakenveina@yahoo.com>
2018-11-16 20:19:15 -0600

    Implemented unit tests for GuildRoleRepository.

---
+++ Modix.Data.Test/Repositories/GuildRoleRepositoryTests.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+using Modix.Data.Models.Core;
+using Modix.Data.Repositories;
+using Modix.Data.Test.TestData;
+
+using NSubstitute;
+using NUnit.Framework;
+using Shouldly;
+
+namespace Modix.Data.Test.Repositories
+{
+    [TestFixture]
+    public class GuildRoleRepositoryTests
+    {
+        #region Test Context
+
+        private static (ModixContext, GuildRoleRepository) BuildTestContext()
+        {
+            var modixContext = TestDataContextFactory.BuildTestDataContext(x =>
+            {
+                x.GuildRoles.AddRange(GuildRoles.Entities.Clone());
+            });
+
+            var uut = new GuildRoleRepository(modixContext);
+
+            return (modixContext, uut);
+        }
+
+        #endregion Test Context
+
+        #region Constructor() Tests
+
+        [Test]
+        public void Constructor_Always_InvokesBaseConstructor()
+        {
+            (var modixContext, var uut) = BuildTestContext();
+
+            uut.ModixContext.ShouldBeSameAs(modixContext);
+        }
+
+        #endregion Constructor() Tests
+
+        #region BeginCreateTransactionAsync() Tests
+
+        [Test]
+        [NonParallelizable]
+        public async Task BeginCreateTransactionAsync_CreateTransactionIsInProgress_WaitsForCompletion()
+        {
+            (var modixContext, var uut) = BuildTestContext();
+
+            var existingTransaction = await uut.BeginCreateTransactionAsync();
+
+            var result = uut.BeginCreateTransactionAsync();
+
+            result.IsCompleted.ShouldBeFalse();
+
+            existingTransaction.Dispose();
+            (await result).Dispose();
+        }
+
+        [Test]
+        [NonParallelizable]
+        public async Task BeginCreateTransactionAsync_CreateTransactionIsNotInProgress_ReturnsImmediately()
+        {
+            (var modixContext, var uut) = BuildTestContext();
+
+            var result = uut.BeginCreateTransactionAsync();
+
+            result.IsCompleted.ShouldBeTrue();
+
+            (await result).Dispose();
+        }
+
+        [Test]
+        [NonParallelizable]
+        public async Task BeginCreateTransactionAsync_Always_TransactionIsForContextDatabase()
+        {
+            (var modixContext, var uut) = BuildTestContext();
+
+            var database = Substitute.ForPartsOf<DatabaseFacade>(modixContext);
+            modixContext.Database.Returns(database);
+
+            using (var transaction = await uut.BeginCreateTransactionAsync()) { }
+
+            await database.ShouldHaveReceived(1)
+                .BeginTransactionAsync();
+        }
+
+        #endregion BeginCreateTransactionAsync() Tests
+
+        #region CreateAsync() Tests
+
+        [Test]
+        public async Task CreateAsync_DataIsNull_ThrowsException()
+        {
+            (var modixContext, var uut) = BuildTestContext();
+
+            await Should.ThrowAsync<ArgumentNullException>(uut.CreateAsync(null));
+
+            await modixContext.ShouldNotHaveReceived()
+                .SaveChangesAsync();
+        }
+
+        [TestCaseSource(nameof(NewGuildRoleCreationTestCases))]
+        public async Task CreateAsync_GuildRoleDoesNotExist_InsertsGuildRole(GuildRoleCreationData data)
+        {
+            (var modixContext, var uut) = BuildTestContext();
+
+            await uut.CreateAsync(data);
+
+            modixContext.GuildRoles.ShouldContain(x => x.RoleId == data.RoleId);
+            var role = modixContext.GuildRoles.First(x => x.RoleId == data.RoleId);
+
+            role.GuildId.ShouldBe(data.GuildId);
+            role.Name.ShouldBe(data.Name);
+            role.Position.ShouldBe(data.Position);
+
+            await modixContext.ShouldHaveReceived(1)
+                .SaveChangesAsync();
+        }
+
+        [TestCaseSource(nameof(ExistingGuildRoleCreationTestCases))]
+        public async Task CreateAsync_GuildRoleExists_ThrowsException(GuildRoleCreationData data)
+        {
+            (var modixContext, var uut) = BuildTestContext();
+
+            await Should.ThrowAsync<InvalidOperationException>(uut.CreateAsync(data));
+
+            await modixContext.ShouldNotHaveReceived()
+                .SaveChangesAsync();
+        }
+
+        #endregion CreateAsync() Tests
+
+        #region TryUpateAsync() Tests
+
+        [Test]
+        public async Task TryUpdateAsync_UpdateActionIsNull_ThrowsException()
+        {
+            (var modixContext, var uut) = BuildTestContext();
+
+            await Should.ThrowAsync<ArgumentNullException>(async () =>
+                await uut.TryUpdateAsync(1, null));
+
+            await modixContext.ShouldNotHaveReceived()
+                .SaveChangesAsync();
+        }
+
+        [TestCaseSource(nameof(ExistingGuildRoleIds))]
+        public async Task TryUpdateAsync_GuildRoleExists_UpdatesGuildRoleAndReturnsTrue(ulong roleId)
+        {
+            (var modixContext, var uut) = BuildTestContext();
+
+            var guildRole = modixContext.GuildRoles.Single(x => x.RoleId == roleId);
+
+            var mutatedData = new GuildRoleMutationData()
+            {
+                Name = "UpdatedRole",
+                Position = 20
+            };
+
+            var result = await uut.TryUpdateAsync(roleId, data =>
+            {
+                data.Name.ShouldBe(guildRole.Name);
+                data.Position.ShouldBe(guildRole.Position);
+
+                data.Name = mutatedData.Name;
+                data.Position = mutatedData.Position;
+            });
+
+            result.ShouldBeTrue();
+
+            guildRole.Name.ShouldBe(mutatedData.Name);
+            guildRole.Position.ShouldBe(mutatedData.Position);
+
+            await modixContext.ShouldHaveReceived(1)
+                .SaveChangesAsync();
+        }
+
+        [TestCaseSource(nameof(NewGuildRoleIds))]
+        public async Task TryUpdateAsync_GuildRoleDoesNotExist_DoesNotPerformUpdateAndReturnsFalse(ulong roleId)
+        {
+            (var modixContext, var uut) = BuildTestContext();
+
+            var updateAction = Substitute.For<Action<GuildRoleMutationData>>();
+
+            var result = await uut.TryUpdateAsync(roleId, updateAction);
+
+            result.ShouldBeFalse();
+
+            updateAction.ShouldNotHaveReceived()
+                .Invoke(Arg.Any<GuildRoleMutationData>());
+
+            await modixContext.ShouldNotHaveReceived()
+                .SaveChangesAsync();
+        }
+
+        #endregion TryUpateAsync() Tests
+
+        #region Test Data
+
+        public static readonly IEnumerable<TestCaseData> NewGuildRoleCreationTestCases
+            = GuildRoles.NewCreations
+                .Select(x => new TestCaseData(x)
+                    .SetName($"{{m}}({x.RoleId})"));
+
+        public static readonly IEnumerable<TestCaseData> ExistingGuildRoleCreationTestCases
+            = GuildRoles.ExistingCreations
+                .Select(x => new TestCaseData(x)
+                    .SetName($"{{m}}({x.RoleId})"));
+
+        public static readonly IEnumerable<ulong> NewGuildRoleIds
+            = GuildRoles.NewCreations
+                .Select(x => x.RoleId);
+
+        public static readonly IEnumerable<ulong> ExistingGuildRoleIds
+            = GuildRoles.ExistingCreations
+                .Select(x => x.RoleId);
+
+        #endregion Test Data
+    }
+}
--- Modix.Data.Test/TestDataContextFactory.cs
+++ Modix.Data.Test/TestDataContextFactory.cs
@@ -10,6 +10,28 @@ namespace Modix.Data.Test
 {
     public static class TestDataContextFactory
     {
+        public static ModixContext BuildTestDataContext(Action<ModixContext> initializeAction = null)
+        {
+            var modixContext = Substitute.ForPartsOf<ModixContext>(new DbContextOptionsBuilder<ModixContext>()
+                .UseInMemoryDatabase((++_databaseName).ToString())
+                .ConfigureWarnings(warnings =>
+                {
+                    warnings.Ignore(InMemoryEventId.TransactionIgnoredWarning);
+                })
+                .Options);
+
+            if (!(initializeAction is null))
+            {
+                initializeAction.Invoke(modixContext);
+
+                modixContext.SaveChanges();
+
+                modixContext.ClearReceivedCalls();
+            }
+
+            return modixContext;
+        }
+
         public static async Task<ModixContext> BuildTestDataContextAsync(Func<ModixContext, Task> initializeAction = null)
         {
             var modixContext = Substitute.ForPartsOf<ModixContext>(new DbContextOptionsBuilder<ModixContext>()
