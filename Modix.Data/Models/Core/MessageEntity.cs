4b13a4e5b59bc69c39e9b01a1fbfffce2cf59954
Justin Rusbatch <jrusbatch@gmail.com>
2018-11-19 13:58:27 -0500

    Create MessageEntity

---
+++ Modix.Data/Models/Core/MessageEntity.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+using Modix.Data.Utilities;
+
+namespace Modix.Data.Models.Core
+{
+    public class MessageEntity
+    {
+        [Required]
+        public ulong Id { get; set; }
+
+        [Required]
+        public ulong GuildId { get; set; }
+
+        [Required]
+        public ulong ChannelId { get; set; }
+
+        [Required]
+        public ulong AuthorId { get; set; }
+
+        [Required]
+        public DateTimeOffset Timestamp { get; set; }
+
+        [OnModelCreating]
+        internal static void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<MessageEntity>()
+                .HasKey(x => x.Id);
+
+            modelBuilder.Entity<MessageEntity>()
+                .Property(x => x.Id)
+                .HasColumnType<ulong>("numeric(20)");
+
+            modelBuilder.Entity<MessageEntity>()
+                .Property(x => x.GuildId)
+                .HasColumnType<ulong>("numeric(20)");
+
+            modelBuilder.Entity<MessageEntity>()
+                .Property(x => x.ChannelId)
+                .HasColumnType<ulong>("numeric(20)");
+
+            modelBuilder.Entity<MessageEntity>()
+                .Property(x => x.AuthorId)
+                .HasColumnType<ulong>("numeric(20)");
+
+            modelBuilder.Entity<MessageEntity>()
+                .HasIndex(x => new { x.GuildId, x.AuthorId });
+        }
+    }
+}
