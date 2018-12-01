32c23e7381bb838b05ef49df9d5275446bae40da
JakenVeina <jakenveina@yahoo.com>
2018-12-01 01:28:32 -0600

    Implemented a customized IMediator framework, for allowing for "dispatching" operations, which are marshalled to a new logical application thread.

---
+++ Modix.Common/Messaging/INotificationHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Modix.Common.Messaging
+{
+    /// <summary>
+    /// Describes an object that receives and handles application-wide notifications from an <see cref="IMediator"/>.
+    /// </summary>
+    /// <typeparam name="TNotification">The type of notification that this object handles.</typeparam>
+    public interface INotificationHandler<TNotification> where TNotification : INotification
+    {
+        /// <summary>
+        /// Handles a given notification.
+        /// </summary>
+        /// <param name="notification">The notification data to be handled.</param>
+        /// <param name="cancellationToken">A token to cancel the asynchronous operation.</param>
+        /// <returns>A <see cref="Task"/> that will complete when the operation has completed.</returns>
+        Task HandleNotificationAsync(TNotification notification, CancellationToken cancellationToken = default);
+    }
+}
