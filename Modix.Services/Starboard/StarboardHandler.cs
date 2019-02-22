0ef6b70ed84a0923259e3cd044d9d8051f9ec59d
calledude <22471295+calledude@users.noreply.github.com>
2019-02-22 16:50:44 +0100

    Added starboard service/handler class

---
+++ Modix.Services/Starboard/StarboardHandler.cs
@@ -0,0 +1,77 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Modix.Services.Messages.Discord;
+using Modix.Services.Quote;
+using Modix.Services.Core;
+using System;
+using Modix.Data.Models.Core;
+
+namespace Modix.Services.Starboard
+{
+
+    public class StarboardHandler :
+        INotificationHandler<ReactionAdded>,
+        INotificationHandler<ReactionRemoved>
+    {
+        private StarboardService _service;
+        private IQuoteService _quoteService;
+        private IDesignatedChannelService _designatedChannelService;
+        public StarboardHandler(StarboardService service, IQuoteService quoteService, IDesignatedChannelService designatedChannelService)
+        {
+            _service = service;
+            _quoteService = quoteService;
+            _designatedChannelService = designatedChannelService;
+        }
+        
+        public async Task Handle(ReactionAdded notification, CancellationToken cancellationToken)
+        {
+            var reaction = notification.Reaction;
+            if(!await _service.IsStarReaction(reaction))
+            {
+                return;
+            }
+
+            var message = await notification.Message.GetOrDownloadAsync();
+            if (!(message.Channel is IGuildChannel channel))
+            {
+                return;
+            }
+
+            if (await _service.IsAboveReactionThreshold(message, notification))
+            {
+
+                //Get existing message from starboardservice?
+                //if != null -> edit with returned message (selfmessage)?
+                var embed = _quoteService
+                    .BuildQuoteEmbed(message, message.Author)
+                    .AddField("Posted by", message.Author.Mention, true)
+                    .WithColor(new Color(255, 234, 174))
+                    .Build();
+
+                var quoteUrl = await _service.BuildQuoteUrl(channel, message);
+                await _designatedChannelService.SendToDesignatedChannelsAsync(
+                    channel.Guild,
+                    DesignatedChannelType.Starboard,
+                    $"**{await _service.GetReactionCount(message, notification)}** {await GetStarEmote(message, notification)} {quoteUrl}",
+                    embed);
+            }
+        }
+
+        public async Task<string> GetStarEmote(IUserMessage message, ReactionAdded notification)
+        {
+            var reactionCount = await _service.GetReactionCount(message, notification);
+            return reactionCount >= 5 ? _service.GreaterEmote : _service.ReactionEmote;
+        }
+
+        public Task Handle(ReactionRemoved notification, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();  }
+            //=> ModifyRatings(notification.Message, notification.Reaction);
+
+    }
+}
---
+++ Modix.Services/Starboard/StarboardService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using Modix.Services.Core;
+using Modix.Services.Messages.Discord;
+
+namespace Modix.Services.Starboard
+{
+    public class StarboardService
+    {
+        private const string _baseQuoteUrl = "https://discordapp.com/channels";
+        public string ReactionEmote { get; } = "⭐";
+        public string GreaterEmote { get; } = "🌟";
+
+        private IDiscordClient _discordClient;
+
+        public StarboardService(IDiscordClient discordClient, IDesignatedChannelService designatedChannelService)
+        {
+            _discordClient = discordClient;
+        }
+
+        //TODO: Function<bool>: => Starred message exists in designated starboard channel
+
+        //TODO: Function: => Delete starred message in designated starboard channel
+
+        //TODO: Function: => Update starred message in designated starboard channel
+
+
+        public Task<bool> IsStarReaction(SocketReaction reaction)
+            => Task.FromResult(reaction.Emote.Name == ReactionEmote);
+
+        public Task<int> GetReactionCount(IUserMessage message, ReactionAdded reaction)
+            => Task.FromResult(message.Reactions[reaction.Reaction.Emote].ReactionCount);
+
+        public async Task<bool> IsAboveReactionThreshold(IUserMessage message, ReactionAdded reaction)
+        {
+            return await GetReactionCount(message, reaction) >= 2;
+        }
+
+        public Task<string> BuildQuoteUrl(IGuildChannel channel, IMessage message)
+            => Task.FromResult(string.Join("/", _baseQuoteUrl, channel.Guild.Id, channel.Id, message.Author.Id));
+    }
+}
