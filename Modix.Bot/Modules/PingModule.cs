c11649a3d2e49437372bc33c5d8e97f87c2f8fe3
Scott-Caldwell <sccaldwell92@gmail.com>
2019-03-05 09:04:40 -0500

    Add latency to the ping response

--- Modix.Bot/Modules/PingModule.cs
+++ Modix.Bot/Modules/PingModule.cs
@@ -1,12 +1,20 @@
 ﻿using System.Threading.Tasks;
 using Discord.Commands;
+using Discord.WebSocket;
 
 namespace Modix.Modules
 {
     public class PingModule : ModuleBase
     {
+        public PingModule(DiscordSocketClient discordClient)
+        {
+            DiscordClient = discordClient;
+        }
+
         [Command("ping")]
         public Task Ping() =>
-            ReplyAsync("Pong!");
+            ReplyAsync($"Pong! ({DiscordClient.Latency} ms)");
+
+        protected DiscordSocketClient DiscordClient { get; }
     }
 }
