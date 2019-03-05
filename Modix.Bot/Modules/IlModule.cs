3404983090f732843ebe1eeb6dd5801e990bc969
calledude <22471295+calledude@users.noreply.github.com>
2019-03-05 23:32:56 +0100

    Refactored quoteservice/autoremovemessage behavior-related stuff

--- Modix.Bot/Modules/IlModule.cs
+++ Modix.Bot/Modules/IlModule.cs
@@ -86,6 +86,8 @@ namespace Modix.Modules
 
             var embed = await BuildEmbed(guildUser, code, parsedResult);
 
+            await _autoRemoveMessageService.RegisterRemovableMessageAsync(message, Context.User, embed);
+
             await message.ModifyAsync(a =>
             {
                 a.Content = string.Empty;
@@ -93,8 +95,6 @@ namespace Modix.Modules
             });
 
             await Context.Message.DeleteAsync();
-
-            await _autoRemoveMessageService.RegisterRemovableMessageAsync(message, Context.User);
         }
 
         private async Task<EmbedBuilder> BuildEmbed(SocketGuildUser guildUser, string code, string result)
@@ -113,8 +113,6 @@ namespace Modix.Modules
                  .WithValue(Format.Code(result.TruncateTo(990), "asm")));
 
             await embed.UploadToServiceIfBiggerThan(result, "asm", 990, _pasteService);
-
-            embed.WithFooter("React with ❌ to remove this embed.");
 
             return embed;
         }
--- Modix.Services/Quote/MessageLinkBehavior.cs
+++ Modix.Services/Quote/MessageLinkBehavior.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -7,7 +6,6 @@ using Discord;
 using Discord.WebSocket;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Modix.Services.AutoRemoveMessage;
 
 namespace Modix.Services.Quote
 {
@@ -44,18 +42,19 @@ namespace Modix.Services.Quote
 
         private async Task OnMessageReceivedAsync(SocketMessage message)
         {
-            if (!(message is SocketUserMessage userMessage) ||
-                !(userMessage.Author is SocketGuildUser guildUser) ||
-                guildUser.IsBot || guildUser.IsWebhook)
+            if (!(message is SocketUserMessage userMessage)
+                || !(userMessage.Author is SocketGuildUser guildUser)
+                || guildUser.IsBot
+                || guildUser.IsWebhook)
             {
                 return;
             }
 
             foreach (Match match in Pattern.Matches(message.Content))
             {
-                if (ulong.TryParse(match.Groups["GuildId"].Value, out var guildId) &&
-                    ulong.TryParse(match.Groups["ChannelId"].Value, out var channelId) &&
-                    ulong.TryParse(match.Groups["MessageId"].Value, out var messageId))
+                if (ulong.TryParse(match.Groups["GuildId"].Value, out var guildId)
+                    && ulong.TryParse(match.Groups["ChannelId"].Value, out var channelId)
+                    && ulong.TryParse(match.Groups["MessageId"].Value, out var messageId))
                 {
                     try
                     {
@@ -65,10 +64,10 @@ namespace Modix.Services.Quote
                             channel is ISocketMessageChannel messageChannel)
                         {
                             var msg = await messageChannel.GetMessageAsync(messageId);
-                            if (msg == null || IsQuote(msg))
-                                return;
+                            if (msg == null) return;
 
-                            await SendQuoteEmbedAsync(msg, guildUser, userMessage.Channel);
+                            var success = await SendQuoteEmbedAsync(msg, guildUser, userMessage.Channel);
+                            if(success) await userMessage.DeleteAsync();
                         }
                     }
                     catch (Exception ex)
@@ -79,31 +78,20 @@ namespace Modix.Services.Quote
             }
         }
 
-        private async Task SendQuoteEmbedAsync(IMessage message, SocketGuildUser quoter, ISocketMessageChannel targetChannel)
+        private async Task<bool> SendQuoteEmbedAsync(IMessage message, SocketGuildUser quoter, ISocketMessageChannel targetChannel)
         {
+            bool success = false;
             await SelfExecuteRequest<IQuoteService>(async quoteService =>
             {
-                var embed = quoteService.BuildQuoteEmbed(message, quoter);
-                if (embed == null) return;
-
-                embed.WithFooter("React with ❌ to remove this embed.");
-                embed.WithTimestamp(message.Timestamp);
-
-                var removableMessage = await targetChannel.SendMessageAsync(string.Empty, embed: embed.Build());
-                await SelfExecuteRequest<IAutoRemoveMessageService>(async messageRemoveService
-                    => await messageRemoveService.RegisterRemovableMessageAsync(removableMessage, quoter));
+                await quoteService.BuildRemovableEmbed(message, quoter,
+                    async (embed) => //If embed building is unsuccessful, this won't execute
+                    {
+                        success = true;
+                        return await targetChannel.SendMessageAsync(embed: embed.Build());
+                    });
             });
-        }
 
-        private bool IsQuote(IMessage message)
-        {
-            var hasQuoteField =
-                message
-                .Embeds?
-                .SelectMany(d=>d.Fields)
-                .Any(d => d.Name == "Quoted by");
-
-            return hasQuoteField.HasValue && hasQuoteField.Value;
+            return success;
         }
     }
 }
