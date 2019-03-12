15a105072de8ec8bce9457f6b399a037cfc8f828
calledude <22471295+calledude@users.noreply.github.com>
2019-03-12 22:54:01 +0100

    Fixed multi-character emojis

--- Modix.Bot/Modules/FunModule.cs
+++ Modix.Bot/Modules/FunModule.cs
@@ -1,5 +1,7 @@
 ﻿using System.IO;
+using System.Linq;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
@@ -25,13 +27,31 @@ namespace Modix.Modules
             if (Emote.TryParse(emoji, out var found))
             {
                 emojiUrl = found.Url;
-            }   
+            }
             else
             {
-                var codepoint = char.ConvertToUtf32(emoji, 0);
-                var codepointHex = codepoint.ToString("X").ToLower();
+                var sb = new StringBuilder();
+                sb.Append("https://raw.githubusercontent.com/twitter/twemoji/gh-pages/2/72x72/");
 
-                emojiUrl = $"https://raw.githubusercontent.com/twitter/twemoji/gh-pages/2/72x72/{codepointHex}.png";
+                for(int i = 0; i < emoji.Length; i++)
+                {
+                    var codepoint = char.ConvertToUtf32(emoji, i);
+                    var codepointHex = codepoint.ToString("x");
+
+                    //ConvertToUtf32() might have parsed an extra character
+                    //Therefore we make sure to skip the next one
+                    //We still need to increment through the string in a char by char manner though since
+                    //some characters used in emojis sometimes only occupy one character (16 bit) in size.
+                    if (codepoint > 0xFFFF)
+                        i++;
+
+                    sb.Append(codepointHex);
+
+                    if (i+1 < emoji.Length) sb.Append("-");
+                }
+
+                sb.Append(".png");
+                emojiUrl = sb.ToString();
             }
 
             try
