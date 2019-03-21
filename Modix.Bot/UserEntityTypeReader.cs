25b70ec46cac5b6fc35a7d93eb922231ad482754
calledude <22471295+calledude@users.noreply.github.com>
2019-03-21 22:37:47 +0100

    Checking both lower and upper bounds before considering it a valid userid

--- Modix.Bot/UserEntityTypeReader.cs
+++ Modix.Bot/UserEntityTypeReader.cs
@@ -30,11 +30,18 @@ namespace Modix
             if (ulong.TryParse(input, out var uid))
             {
                 //Any ulong is technically a valid snowflake, but we try to do some basic validation
-                //by parsing the timestamp (in ms) part out of it - if it's less than or equal to 0, it's
-                //before the Discord epoch of Jan 1, 2015, and thus invalid
+                //by parsing the timestamp (in ms) part out of it - we consider it to be an invalid snowflake if:
+                // - it's less than or equal to the discord epoch baseline
+                // - it's greater than or equal to the current timestamp
                 var snowflakeTimestamp = (long)(uid >> 22);
+                const long discordEpochUnixTime = 1420070400000;
 
-                if (snowflakeTimestamp <= 0)
+                //Jan 1, 2015
+                var discordEpoch = DateTimeOffset.FromUnixTimeMilliseconds(discordEpochUnixTime);
+
+                //The supposed timestamp
+                var snowFlakeDateTime = DateTimeOffset.FromUnixTimeMilliseconds(snowflakeTimestamp + discordEpochUnixTime);
+                if (snowFlakeDateTime <= discordEpoch || snowFlakeDateTime >= DateTimeOffset.UtcNow)
                 {
                     return TypeReaderResult.FromError(CommandError.ParseFailed, "Snowflake was almost certainly invalid.");
                 }
