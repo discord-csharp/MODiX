e0c6822cda253df6c89563bf8e5750850560d064
calledude <22471295+calledude@users.noreply.github.com>
2019-03-24 22:18:50 +0100

    Laid groundwork for emojistats module/service

---
+++ Modix.Bot/Modules/EmojistatsModule.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Humanizer;
+using Modix.Services.EmojiStats;
+
+namespace Modix.Modules
+{
+    public class EmojiStatsModule : ModuleBase
+    {
+        private readonly IEmojiStatsService _emojiStatsService;
+
+        public EmojiStatsModule(IEmojiStatsService emojiStatsService)
+        {
+            _emojiStatsService = emojiStatsService;
+        }
+
+        [Command("emojistats")]
+        [Summary("Gets usage stats for all emojis in the current guild or a specific emoji if provided.")]
+        public async Task Emojistats(
+            [Summary("The emote to retrieve information about, if any.")]
+                IEmote emote = null)
+        {
+            var guildId = Context.Guild.Id;
+
+            var emojiUsageAllTime = await _emojiStatsService.GetEmojiSummaries(emote, guildId, false);
+            var emojiUsage30 = await _emojiStatsService.GetEmojiSummaries(emote, guildId, true);
+
+            var emojiCountsAllTime = _emojiStatsService
+                .GetCountFromSummary(emojiUsageAllTime)
+                .OrderByDescending(x => x.Value);
+
+            var emojiCounts30 = _emojiStatsService.GetCountFromSummary(emojiUsage30);
+
+            var embed = new EmbedBuilder();
+            var totalEmojiUsage = _emojiStatsService.AggregateUsage(emojiCountsAllTime);
+
+            var oldestTimestamp = _emojiStatsService.GetOldestSummaryTimeStamp(emojiUsage30);
+
+            var divider = (DateTime.UtcNow - oldestTimestamp).Days;
+            divider = divider == 0 ? 1 : divider;
+
+            for (int i = 0; i < emojiCountsAllTime.Count(); i++)
+            {
+                var kvp = emojiCountsAllTime.ElementAt(i);
+                var emojiFormatted = Format.Url(kvp.Key.ToString(), kvp.Key.Url);
+                var usage = kvp.Value;
+                var percentUsage = (100 * (double) usage / totalEmojiUsage).ToString("0.00");
+
+                double usageLast30 = 0;
+
+                if (emojiCounts30.TryGetValue(kvp.Key, out var val))
+                {
+                    usageLast30 = (double)val / divider;
+                }
+
+                embed.Description += $"{i+1}. {emojiFormatted} ({"use".ToQuantity(usage)}) ({percentUsage}%), {usageLast30.ToString("0.00/day")}\n";
+            }
+
+            await ReplyAsync(embed: embed.Build());
+        }
+    }
+}
---
+++ Modix.Services/EmojiStats/EmojiStatsService.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Discord;
+using Modix.Data.Models;
+using Modix.Data.Models.Emoji;
+using Modix.Data.Repositories;
+
+namespace Modix.Services.EmojiStats
+{
+    public interface IEmojiStatsService
+    {
+        Task<IReadOnlyCollection<EmojiSummary>> GetEmojiSummaries(IEmote emote, ulong guildId, bool monthly);
+
+        IReadOnlyDictionary<EphemeralEmoji, int> GetCountFromSummary(IEnumerable<EmojiSummary> emojiSummaries);
+
+        int AggregateUsage(IEnumerable<KeyValuePair<EphemeralEmoji, int>> emojis);
+
+        DateTimeOffset GetOldestSummaryTimeStamp(IEnumerable<EmojiSummary> emojiSummaries);
+    }
+
+    public class EmojiStatsService : IEmojiStatsService
+    {
+        private readonly IEmojiRepository _emojiRepository;
+
+        public EmojiStatsService(IEmojiRepository emojiRepository)
+        {
+            _emojiRepository = emojiRepository;
+        }
+
+
+        public async Task<IReadOnlyCollection<EmojiSummary>> GetEmojiSummaries(IEmote emote, ulong guildId, bool monthly)
+        {
+            var criteria = new EmojiSearchCriteria()
+            {
+                EmojiName = emote?.Name,
+                GuildId = guildId,
+            };
+
+            if (monthly)
+            {
+                criteria.TimestampRange = new DateTimeOffsetRange()
+                {
+                    From = DateTimeOffset.UtcNow - TimeSpan.FromDays(30),
+                    To = DateTimeOffset.UtcNow
+                };
+            }
+
+            return await _emojiRepository.SearchSummariesAsync(criteria);
+        }
+
+        public IReadOnlyDictionary<EphemeralEmoji, int> GetCountFromSummary(IEnumerable<EmojiSummary> emojiSummaries)
+        {
+            return emojiSummaries
+                .GroupBy(x => new { x.Emoji.Id, x.Emoji.Name })
+                .ToDictionary(x => EphemeralEmoji.FromRawData(x.Key.Name, x.Key.Id),
+                              x => x.Count(),
+                              new EphemeralEmoji.EqualityComparer());
+        }
+
+        public int AggregateUsage(IEnumerable<KeyValuePair<EphemeralEmoji,int>> emojis)
+        {
+            return emojis
+                .Select(x => x.Value)
+                .Aggregate((i, j) => i + j);
+        }
+
+        public DateTimeOffset GetOldestSummaryTimeStamp(IEnumerable<EmojiSummary> emojiSummaries)
+        {
+            return emojiSummaries
+                .OrderByDescending(x => x.Timestamp)
+                .First()
+                .Timestamp;
+        }
+    }
+}
