8fdebb2a0c2c74233590a6e2be74eba174a34cdd
calledude <22471295+calledude@users.noreply.github.com>
2019-04-01 01:10:42 +0200

    Removed unnecessary paragraph

--- Modix/ClientApp/src/components/UserLookup/UserSearch.vue
+++ Modix/ClientApp/src/components/UserLookup/UserSearch.vue
@@ -11,7 +11,6 @@
                         </template>
                     </Autocomplete>
                 </div>
-                <p class="help is-danger"></p>
             </div>
         </div>
     </section>
