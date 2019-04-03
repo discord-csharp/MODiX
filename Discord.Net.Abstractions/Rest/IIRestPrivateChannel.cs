550dee440114e1a7262e06355dc8f5af8f47be05
JakenVeina <jakenveina@yahoo.com>
2019-04-02 23:42:43 -0500

    Fixed a design flaw in the abstraction architecture for interfaces that need to be abstracted, which didn't properly expose all underlying functionality of abstracted objects.

--- Discord.Net.Abstractions/Rest/IIRestPrivateChannel.cs
+++ Discord.Net.Abstractions/Rest/IIRestPrivateChannel.cs
@@ -13,57 +13,6 @@ namespace Discord.Rest
     }
 
     /// <summary>
-    /// Provides an abstraction wrapper layer around a <see cref="Rest.IRestPrivateChannel"/>, through the <see cref="IIRestPrivateChannel"/> interface.
-    /// </summary>
-    public class IRestPrivateChannelAbstraction : IIRestPrivateChannel
-    {
-        /// <summary>
-        /// Constructs a new <see cref="IRestPrivateChannelAbstraction"/> around an existing <see cref="Rest.IRestPrivateChannel"/>.
-        /// </summary>
-        /// <param name="iRestPrivateChannel">The value to use for <see cref="Rest.IRestPrivateChannel"/>.</param>
-        /// <exception cref="ArgumentNullException">Throws for <paramref name="iRestPrivateChannel"/>.</exception>
-        public IRestPrivateChannelAbstraction(IRestPrivateChannel iRestPrivateChannel)
-        {
-            IRestPrivateChannel = iRestPrivateChannel ?? throw new ArgumentNullException(nameof(iRestPrivateChannel));
-        }
-
-        /// <inheritdoc />
-        public DateTimeOffset CreatedAt
-            => IRestPrivateChannel.CreatedAt;
-
-        /// <inheritdoc />
-        public ulong Id
-            => IRestPrivateChannel.Id;
-
-        /// <inheritdoc />
-        public string Name
-            => IRestPrivateChannel.Name;
-
-        /// <inheritdoc />
-        public IReadOnlyCollection<IRestUser> Recipients
-            => IRestPrivateChannel.Recipients
-                .Select(RestUserAbstractionExtensions.Abstract)
-                .ToArray();
-
-        /// <inheritdoc />
-        IReadOnlyCollection<IUser> IPrivateChannel.Recipients
-            => (IRestPrivateChannel as IPrivateChannel).Recipients;
-
-        /// <inheritdoc />
-        public Task<IUser> GetUserAsync(ulong id, CacheMode mode = CacheMode.AllowDownload, RequestOptions options = null)
-            => IRestPrivateChannel.GetUserAsync(id, mode, options);
-
-        /// <inheritdoc />
-        public IAsyncEnumerable<IReadOnlyCollection<IUser>> GetUsersAsync(CacheMode mode = CacheMode.AllowDownload, RequestOptions options = null)
-            => IRestPrivateChannel.GetUsersAsync(mode, options);
-
-        /// <summary>
-        /// The existing <see cref="Rest.IRestPrivateChannel"/> being abstracted.
-        /// </summary>
-        protected IRestPrivateChannel IRestPrivateChannel { get; }
-    }
-
-    /// <summary>
     /// Contains extension methods for abstracting <see cref="IRestPrivateChannel"/> objects.
     /// </summary>
     public static class IRestPrivateChannelAbstractionExtensions
@@ -75,6 +24,9 @@ namespace Discord.Rest
         /// <exception cref="ArgumentNullException">Throws for <paramref name="iRestPrivateChannel"/>.</exception>
         /// <returns>An <see cref="IIRestPrivateChannel"/> that abstracts <paramref name="iRestPrivateChannel"/>.</returns>
         public static IIRestPrivateChannel Abstract(this IRestPrivateChannel iRestPrivateChannel)
-            => new IRestPrivateChannelAbstraction(iRestPrivateChannel);
+            => (iRestPrivateChannel is null) ? throw new ArgumentNullException(nameof(iRestPrivateChannel))
+                : (iRestPrivateChannel is RestDMChannel restDMChannel) ? restDMChannel.Abstract() as IIRestPrivateChannel
+                : (iRestPrivateChannel is RestGroupChannel restGroupChannel) ? restGroupChannel.Abstract() as IIRestPrivateChannel
+                : throw new NotSupportedException($"{nameof(IRestPrivateChannel)} type {iRestPrivateChannel.GetType().FullName} is not supported");
     }
 }
