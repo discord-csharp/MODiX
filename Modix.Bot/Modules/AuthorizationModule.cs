7a7a714b972c89cc6b390735d56d27c1f10ee928
Sutarodo <joshuacrowe@outlook.com>
2019-04-13 00:35:08 +0100

    Amendments for PR

--- Modix.Bot/Modules/AuthorizationModule.cs
+++ Modix.Bot/Modules/AuthorizationModule.cs
@@ -1,11 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 using Discord;
 using Discord.Commands;
 
-using Modix.Bot.Extensions;
 using Modix.Data.Models.Core;
 using Modix.Services.CommandHelp;
 using Modix.Services.Core;
@@ -28,26 +28,22 @@ namespace Modix.Modules
         [Command("claims")]
         [Summary("Lists the currently assigned claims for the calling user, or given user")]
         public async Task ClaimsAsync(
-
             [Summary("User whom to list the claims for, if any")]
-            IGuildUser user = null)
-
+                IGuildUser user = null)
         {
             // If a user was provided, get their claims, otherwise get the caller's claims.
             var claims = user is null
                 ? _authorizationService.CurrentClaims
                 : await _authorizationService.GetGuildUserClaimsAsync(user);
 
-            await ReplyWithClaimsAsync(claims.ToList());
+            await ReplyWithClaimsAsync(claims);
         }
 
         [Command("claims")]
         [Summary("Lists the currently assigned claims for the given role.")]
         public async Task ClaimsAsync(
-
             [Summary("Role for which to list claims of.")]
-            IRole role)
-
+                IRole role)
         {
             var claims = await _authorizationService.GetGuildRoleClaimsAsync(role);
             await ReplyWithClaimsAsync(claims);
@@ -55,36 +51,54 @@ namespace Modix.Modules
 
         [Command("claims add")]
         [Summary("Adds a claim to the given role")]
-        public Task AddClaim(
-
+        public Task AddClaimAsync(
             [Summary("Claim to be added")]
-            AuthorizationClaim claim,
-
+                AuthorizationClaim claim,
             [Summary("Access of a claim, whether granted or denied")]
-            ClaimMappingType type,
-
+                ClaimMappingType type,
             [Summary("Role which to add the claim to")]
-            IRole role)
-
+                IRole role)
         {
             return _authorizationService.AddClaimMappingAsync(role, type, claim);
         }
 
+        [Command("claims add")]
+        [Summary("Adds a claim to the given user")]
+        public Task AddClaimAsync(
+            [Summary("Claim to be added")]
+                AuthorizationClaim claim,
+            [Summary("Access of a claim, whether granted or denied")]
+                ClaimMappingType type,
+            [Summary("User to add the claim to")]
+                IGuildUser user)
+        {
+            return _authorizationService.AddClaimMappingAsync(user, type, claim);
+        }
+
         [Command("claims remove")]
         [Summary("Removes a claim from the given role")]
-        public Task RemoveClaim(
-
+        public Task RemoveClaimAsync(
             [Summary("Claim to be removed")]
-            AuthorizationClaim claim,
-
+                AuthorizationClaim claim,
             [Summary("Access of the claim, whether granted or denied")]
-            ClaimMappingType type,
-
+                ClaimMappingType type,
             [Summary("Role from which the claim is to be removed")]
-            IRole role)
-
+                IRole role)
         {
             return _authorizationService.RemoveClaimMappingAsync(role, type, claim);
+        }
+
+        [Command("claims remove")]
+        [Summary("Removes a claim from the given user")]
+        public Task RemoveClaimAsync(
+            [Summary("Claim to be added")]
+                AuthorizationClaim claim,
+            [Summary("Access of a claim, whether granted or denied")]
+                ClaimMappingType type,
+            [Summary("User to add the claim to")]
+                IGuildUser user)
+        {
+            return _authorizationService.RemoveClaimMappingAsync(user, type, claim);
         }
 
         private async Task ReplyWithClaimsAsync(IReadOnlyCollection<AuthorizationClaim> claims)
@@ -95,7 +109,11 @@ namespace Modix.Modules
                 return;
             }
 
-            await ReplyAsync(claims.ToMessageableList());
+            // Turn the claims into strings, then put them one under another in a code block.
+            var stringifiedClaims = claims.Select(x => x.ToString());
+            var joinedClaims = string.Join(Environment.NewLine, stringifiedClaims);
+
+            await ReplyAsync(Format.Code(joinedClaims));
         }
     }
 }
