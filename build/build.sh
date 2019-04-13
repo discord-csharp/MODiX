c53f65832f295f63ece11816dbeee1d2f8c14a5d
John Mazouri <me@jmazouri.com>
2019-04-12 21:24:13 -0400

    Explicitly build as self-contained to avoid runtime dependency issues

--- build/build.sh
+++ build/build.sh
@@ -3,4 +3,4 @@ apt-get update && apt install curl -y
 curl -sL https://deb.nodesource.com/setup_10.x | bash -
 apt-get install nodejs -y
 
-dotnet publish Modix.sln -c Release -r linux-x64 -o /app
+dotnet publish Modix.sln -c Release -r linux-x64 --self-contained -o /app
