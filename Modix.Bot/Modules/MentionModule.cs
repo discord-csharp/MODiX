98ff6da72a6d68387679bc370bf29ba07e225498
foxtrek64 <foxtrek64@gmail.com>
2019-05-01 00:01:29 -0500

    Merge MentionModule commands using default (null) message arg

--- Modix.Bot/Modules/MentionModule.cs
+++ Modix.Bot/Modules/MentionModule.cs
@@ -22,18 +22,10 @@ namespace Modix.Bot.Modules
         [Summary("Mentions the supplied role.")]
         public async Task MentionAsync(
             [Summary("The role that the user is attempting to mention.")]
-                IRole role)
-            => await MentionService.MentionRoleAsync(role, Context.Channel);
-
-        [Command("mention")]
-        [Alias("@")]
-        [Summary("Mentions the supplied role.")]
-        public async Task MentionAsync(
-            [Summary("The role that the user is attempting to mention.")]
             IRole role,
             [Summary("Message to provide to mentionees. The 'message' argument is ignored by the command.")]
             [Remainder]
-            string message)
+            string message = null)
             => await MentionService.MentionRoleAsync(role, Context.Channel);
 
         internal protected IMentionService MentionService { get; }
