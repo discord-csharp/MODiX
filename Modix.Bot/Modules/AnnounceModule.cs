806882225c4317e86cb56900c8fffc89e05f5dca
Foxtrek_64 <Foxtrek64@users.noreply.github.com>
2019-05-01 09:25:06 -0500

    Fix mentioning, add announcements (#470)
    * Fix mentioning, add announcements
    
    * Fix module name, add command name, spelling correction
    
    * MentionService returns Task<bool>, Add note to MentionModule, add check to AnnounceModule
    
    * Provide clear mention message arg comment, clean up MentionService
    
    * Move remainder attribute to new line in AnnounceModule
    
    * Merge MentionModule commands using default (null) message arg

---
+++ Modix.Bot/Modules/AnnounceModule.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Modix.Services.CommandHelp;
+using Modix.Services.Mentions;
+
+namespace Modix.Bot.Modules
+{
+    [Name("Announcements")]
+    [Summary("Makes an announcement")]
+    [HelpTags("announce", "$")]
+    public class AnnounceModule : ModuleBase
+    {
+        private IMentionService _mentionService { get; }
+
+        public AnnounceModule(IMentionService mentionService)
+        {
+            _mentionService = mentionService;
+        }
+
+        [Command("announce")]
+        [Alias("$")]
+        [Summary("Makes an announcement in the current channel")]
+        public async Task MakeAnnouncementAsync([Summary("The role to mention")] IRole role,
+            [Summary("The message to send as part of the announcement.")]
+            [Remainder]
+            string message)
+        {
+            // Send message
+            if (await _mentionService.MentionRoleAsync(role, Context.Channel, message))
+            {
+                // Clean up
+                await Context.Message.DeleteAsync();
+            }
+        }
+    }
+}
