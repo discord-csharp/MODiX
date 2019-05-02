778e7ca3e0a8cd8a5b80bf7fc80064974917ffb2
foxtrek64 <foxtrek64@gmail.com>
2019-05-01 22:04:53 -0500

    Add descriptions for UriBuilder.ToString overload

--- Modix.Bot/Extensions/UriBuilderExtensions.cs
+++ Modix.Bot/Extensions/UriBuilderExtensions.cs
@@ -6,6 +6,12 @@ namespace Modix.Bot.Extensions
 {
     public static class UriBuilderExtensions
     {
+        /// <summary>
+        /// Returns the display string for the specified <see cref="UriBuilder"/> instance.
+        /// </summary>
+        /// <param name="trimDefaultPort">True to trim the default port; otherwise false.</param>
+        /// <remarks>If <param name="trimDefaultPort" /> is false, the result is the same as running <see cref="UriBuilder.ToString"/>.</remarks>
+        /// <returns></returns>
         public static string ToString(this UriBuilder builder, bool trimDefaultPort)
         {
             if (trimDefaultPort && builder.Uri.IsDefaultPort)
