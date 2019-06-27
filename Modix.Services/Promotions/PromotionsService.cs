09b3dd99b6e7035f048d0285469283a8407eb715
ChaseDR <redmon.chase@gmail.com>
2019-06-27 16:58:27 -0400

    we want 20 days!

--- Modix.Services/Promotions/PromotionsService.cs
+++ Modix.Services/Promotions/PromotionsService.cs
@@ -506,8 +506,8 @@ namespace Modix.Services.Promotions
 
             // JoinedAt is null, when it cannot be obtained
             if (subject.JoinedAt.HasValue)
-                if (subject.JoinedAt.Value.DateTime > (DateTimeOffset.Now - TimeSpan.FromDays(30)))
-                    throw new InvalidOperationException($"{subject.GetFullUsername()} has joined less than 30 days prior");
+                if (subject.JoinedAt.Value.DateTime > (DateTimeOffset.Now - TimeSpan.FromDays(20)))
+                    throw new InvalidOperationException($"{subject.GetFullUsername()} has joined less than 20 days prior");
 
             if (!await CheckIfUserIsRankOrHigherAsync(rankRoles, AuthorizationService.CurrentUserId.Value, targetRankRole.Id))
                 throw new InvalidOperationException($"Creating a promotion campaign requires a rank at least as high as the proposed target rank");
