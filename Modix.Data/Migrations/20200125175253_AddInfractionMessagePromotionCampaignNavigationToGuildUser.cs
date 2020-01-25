d800ae38401a60d620b234be5ff76d62986170fd
Patrick Klaeren <patrick@totalren.com>
2020-01-25 17:53:23 +0000

    Add migration

---
+++ Modix.Data/Migrations/20200125175253_AddInfractionMessagePromotionCampaignNavigationToGuildUser.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace Modix.Data.Migrations
+{
+    public partial class AddInfractionMessagePromotionCampaignNavigationToGuildUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_ChannelId",
+                table: "Messages",
+                column: "ChannelId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Messages_GuildChannels_ChannelId",
+                table: "Messages",
+                column: "ChannelId",
+                principalTable: "GuildChannels",
+                principalColumn: "ChannelId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Messages_GuildUsers_GuildId_AuthorId",
+                table: "Messages",
+                columns: new[] { "GuildId", "AuthorId" },
+                principalTable: "GuildUsers",
+                principalColumns: new[] { "GuildId", "UserId" },
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Messages_GuildChannels_ChannelId",
+                table: "Messages");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Messages_GuildUsers_GuildId_AuthorId",
+                table: "Messages");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Messages_ChannelId",
+                table: "Messages");
+        }
+    }
+}
