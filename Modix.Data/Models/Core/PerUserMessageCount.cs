6c80d44ce13a02be54c5bfceafba5878ecf5a00f
JakenVeina <jakenveina@yahoo.com>
2020-02-21 21:10:01 -0600

    Workaround for bogus tables generated by keyless entities.
    See https://github.com/dotnet/efcore/issues/19972

--- Modix.Data/Models/Core/PerUserMessageCount.cs
+++ Modix.Data/Models/Core/PerUserMessageCount.cs
@@ -1,4 +1,7 @@
-﻿namespace Modix.Data.Models.Core
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Modix.Data.Models.Core
 {
     public class PerUserMessageCount
     {
@@ -11,5 +14,16 @@
         public int MessageCount { get; set; }
 
         public bool IsCurrentUser { get; set; }
+    }
+
+    public class PerUserMessageCountConfiguration
+        : IEntityTypeConfiguration<PerUserMessageCount>
+    {
+        public void Configure(
+                EntityTypeBuilder<PerUserMessageCount> entityTypeBuilder)
+            => entityTypeBuilder
+                .HasNoKey()
+                // Workaround until .NET 5: https://github.com/dotnet/efcore/issues/19972
+                .ToView("No table or view exists for entity type PerUserMessageCount: This type can only be queried with raw SQL (.FromSqlXXX())");
     }
 }
