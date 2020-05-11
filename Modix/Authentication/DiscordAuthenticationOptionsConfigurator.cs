0f0eaf0d66712a68ecfa6eb25f0fcfa0cb38b755
JakenVeina <jakenveina@yahoo.com>
2020-05-10 22:50:56 -0500

    Refactored Modix.Auth.
    Renamed the namespace for clarity.
    Refactored stuff in ModixAuthenticationHandler to use the "proper" purpose-built APIs.
    Refactored use of DiscordSocketClient to IDiscordSocketClient.

---
+++ Modix/Authentication/DiscordAuthenticationOptionsConfigurator.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+using AspNet.Security.OAuth.Discord;
+
+using Modix.Data.Models.Core;
+
+namespace Modix.Authentication
+{
+    [ServiceBinding(ServiceLifetime.Transient)]
+    public class DiscordAuthenticationOptionsConfigurator
+        : IPostConfigureOptions<DiscordAuthenticationOptions>
+    {
+        public DiscordAuthenticationOptionsConfigurator(
+            IOptions<ModixConfig> modixConfig)
+        {
+            _modixConfig = modixConfig.Value;
+        }
+
+        public void PostConfigure(
+            string name,
+            DiscordAuthenticationOptions options)
+        {
+            options.ClientId = _modixConfig.DiscordClientId;
+            options.ClientSecret = _modixConfig.DiscordClientSecret;
+        }
+
+        private readonly ModixConfig _modixConfig;
+    }
+}
---
+++ Modix/Authentication/DiscordAuthenticationSetup.cs
@@ -0,0 +1,29 @@
+﻿using AspNet.Security.OAuth.Discord;
+
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OAuth;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Modix.Authentication
+{
+    public static class DiscordAuthenticationSetup
+    {
+        public static AuthenticationBuilder AddDiscordAuthentication(
+            this AuthenticationBuilder builder)
+        {
+            return builder.AddOAuth<DiscordAuthenticationOptions, DiscordAuthenticationHandler>(DiscordAuthenticationDefaults.AuthenticationScheme, options =>
+            {
+                options.Events = new OAuthEvents()
+                {
+                    OnCreatingTicket = DiscordAuthenticationEventHandlers.OnCreatingTicket,
+                    OnRemoteFailure = DiscordAuthenticationEventHandlers.OnRemoteFailure
+                };
+
+                options.CorrelationCookie.SecurePolicy = CookieSecurePolicy.Always;
+
+                options.ClaimActions.MapJsonKey(claimType: "avatarHash", jsonKey: "avatar");
+            });
+        }
+    }
+}
