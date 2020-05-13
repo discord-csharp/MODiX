508253a689c36f3f8d90705ed95d32e4ef59b7e9
JakenVeina <jakenveina@yahoo.com>
2020-05-12 21:45:45 -0500

    Refactored `PingModule`.
    Pulled logic for endpoints into Modix.Services, to be managed by the DI system.
    Implemented `CancellationToken` usage.
    Replaced usage of `DiscordSocketClient` and `DiscordRestClient` with `IDiscordSocketClient` and `IDiscordRestClient`.

---
+++ Modix.Services/Diagnostics/PingLatencyEndpoint.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Net.NetworkInformation;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Modix.Services.Diagnostics
+{
+    public sealed class PingLatencyEndpoint
+        : ILatencyEndpoint,
+            IDisposable
+    {
+        public PingLatencyEndpoint(
+            string displayName,
+            string url)
+        {
+            _displayName = displayName;
+            _url = url;
+
+            _ping = new Ping();
+        }
+
+        public string DisplayName
+            => _displayName;
+
+        public void Dispose()
+            => _ping.Dispose();
+
+        public async Task<long> GetLatencyAsync(
+            CancellationToken cancellationToken)
+        {
+            cancellationToken.Register(_ping.SendAsyncCancel);
+
+            var reply = await _ping.SendPingAsync(_url);
+
+            return reply.RoundtripTime;
+        }
+
+        private readonly string _displayName;
+        private readonly Ping _ping;
+        private readonly string _url;
+    }
+
+    [ServiceConfigurator]
+    public class PingLatencyEndpointServiceConfigurator
+        : IServiceConfigurator
+    {
+        public void ConfigureServices(
+                IServiceCollection services,
+                IConfiguration configuration)
+            => services
+                .AddSingleton<ILatencyEndpoint>(serviceProvider => new PingLatencyEndpoint("Google",     "8.8.8.8"))
+                .AddSingleton<ILatencyEndpoint>(serviceProvider => new PingLatencyEndpoint("Cloudflare", "1.1.1.1"));
+    }
+}
