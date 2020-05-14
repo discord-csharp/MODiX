4f2da35e4f085215fc3a23880b0d6c996e8c3d79
JakenVeina <jakenveina@yahoo.com>
2020-05-14 01:12:41 -0500

    Implemented a pattern of enums for managing uniqueness of Logging EventId values across the whole project.

--- Modix.Common/Extensions/Microsoft/Extensions/DependencyInjection/DependencyInjectionLogMessages.cs
+++ Modix.Common/Extensions/Microsoft/Extensions/DependencyInjection/DependencyInjectionLogMessages.cs
@@ -2,10 +2,17 @@
 
 using Microsoft.Extensions.Logging;
 
+using Modix.Common;
+
 namespace Microsoft.Extensions.DependencyInjection
 {
     public static class DependencyInjectionLogMessages
     {
+        public enum EventType
+        {
+            ServiceScopeCreated = CommonLogEventType.DependencyInjection + 0x0001
+        }
+
         public static void ServiceScopeCreated(
                 ILogger logger,
                 IServiceScope serviceScope)
@@ -15,7 +22,7 @@ namespace Microsoft.Extensions.DependencyInjection
         private static readonly Action<ILogger, IServiceScope> _serviceScopeCreated
             = LoggerMessage.Define<IServiceScope>(
                     LogLevel.Debug,
-                    new EventId(4001, nameof(ServiceScopeCreated)),
+                    EventType.ServiceScopeCreated.ToEventId(),
                     $"{nameof(IServiceScope)} created: {{ServiceScope}}")
                 .WithoutException();
     }
