bc3d537ea7b3459abe690c9af5b7266aec92450a
JakenVeina <jakenveina@yahoo.com>
2020-05-24 17:21:04 -0500

    Added an additional overload of `LoggerMessage.DefineScope()` that takes 4 type params.
    Copy/pasted source code from https://github.com/dotnet/runtime/tree/master/src/libraries/Microsoft.Extensions.Logging.Abstractions/src to do so.

---
+++ Modix.Common/Extensions/Microsoft/Extensions/Logging/LoggerMessageEx.cs
@@ -0,0 +1,110 @@
+﻿// TODO: Remove if https://github.com/dotnet/runtime/issues/35060 ever gets resolved
+
+#pragma warning disable // Not my code, not my problem
+
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace Microsoft.Extensions.Logging
+{
+    public static class LoggerMessageEx
+    {
+        /// <summary>
+        /// Creates a delegate which can be invoked to create a log scope.
+        /// </summary>
+        /// <typeparam name="T1">The type of the first parameter passed to the named format string.</typeparam>
+        /// <typeparam name="T2">The type of the second parameter passed to the named format string.</typeparam>
+        /// <typeparam name="T3">The type of the third parameter passed to the named format string.</typeparam>
+        /// <typeparam name="T4">The type of the fourth parameter passed to the named format string.</typeparam>
+        /// <param name="formatString">The named format string</param>
+        /// <returns>A delegate which when invoked creates a log scope.</returns>
+        public static Func<ILogger, T1, T2, T3, T4, IDisposable> DefineScope<T1, T2, T3, T4>(string formatString)
+        {
+            var formatter = CreateLogValuesFormatter(formatString, expectedNamedParameterCount: 4);
+
+            return (logger, arg1, arg2, arg3, arg4) => logger.BeginScope(new LogValues<T1, T2, T3, T4>(formatter, arg1, arg2, arg3, arg4));
+        }
+
+        private static LogValuesFormatter CreateLogValuesFormatter(string formatString, int expectedNamedParameterCount)
+        {
+            var logValuesFormatter = new LogValuesFormatter(formatString);
+
+            var actualCount = logValuesFormatter.ValueNames.Count;
+            if (actualCount != expectedNamedParameterCount)
+            {
+                throw new ArgumentException(
+                    $"The format string '{formatString}' does not have the expected number of named parameters.Expected {expectedNamedParameterCount} parameter(s) but found {actualCount} parameter(s).");
+            }
+
+            return logValuesFormatter;
+        }
+
+        private readonly struct LogValues<T0, T1, T2, T3> : IReadOnlyList<KeyValuePair<string, object>>
+        {
+            public static readonly Func<LogValues<T0, T1, T2, T3>, Exception, string> Callback = (state, exception) => state.ToString();
+
+            private readonly LogValuesFormatter _formatter;
+            private readonly T0 _value0;
+            private readonly T1 _value1;
+            private readonly T2 _value2;
+            private readonly T3 _value3;
+
+            public int Count => 5;
+
+            public KeyValuePair<string, object> this[int index]
+            {
+                get
+                {
+                    switch (index)
+                    {
+                        case 0:
+                            return new KeyValuePair<string, object>(_formatter.ValueNames[0], _value0);
+                        case 1:
+                            return new KeyValuePair<string, object>(_formatter.ValueNames[1], _value1);
+                        case 2:
+                            return new KeyValuePair<string, object>(_formatter.ValueNames[2], _value2);
+                        case 3:
+                            return new KeyValuePair<string, object>(_formatter.ValueNames[3], _value3);
+                        case 4:
+                            return new KeyValuePair<string, object>("{OriginalFormat}", _formatter.OriginalFormat);
+                        default:
+                            throw new IndexOutOfRangeException(nameof(index));
+                    }
+                }
+            }
+
+            public LogValues(LogValuesFormatter formatter, T0 value0, T1 value1, T2 value2, T3 value3)
+            {
+                _formatter = formatter;
+                _value0 = value0;
+                _value1 = value1;
+                _value2 = value2;
+                _value3 = value3;
+            }
+
+            private object[] ToArray() => new object[] { _value0, _value1, _value2, _value3 };
+
+            public override string ToString() => _formatter.Format(ToArray());
+
+            public IEnumerator<KeyValuePair<string, object>> GetEnumerator()
+            {
+                for (int i = 0; i < Count; ++i)
+                {
+                    yield return this[i];
+                }
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return GetEnumerator();
+            }
+        }
+    }
+}
