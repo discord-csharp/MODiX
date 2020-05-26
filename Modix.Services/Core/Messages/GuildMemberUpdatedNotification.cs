1d132bf512f2f8239837faf75980d7975f0598b2
JakenVeina <jakenveina@yahoo.com>
2020-05-25 20:29:02 -0500

    Added Messaging plumbing for IDiscordSocketClient.GuildMemberUpdated.

---
+++ Modix.Services/Core/Messages/GuildMemberUpdatedNotification.cs
@@ -0,0 +1,38 @@
+﻿#nullable enable
+
+using Discord.WebSocket;
+
+using Modix.Common.Messaging;
+
+namespace Discord
+{
+    /// <summary>
+    /// Describes an application-wide notification that occurs when <see cref="IBaseSocketClient.GuildMemberUpdated"/> is raised.
+    /// </summary>
+    public class GuildMemberUpdatedNotification
+        : INotification
+    {
+        /// <summary>
+        /// Constructs a new <see cref="GuildMemberUpdatedNotification"/> from the given values.
+        /// </summary>
+        /// <param name="oldMember">The value to use for <see cref="OldMember"/>.</param>
+        /// <param name="newMember">The value to use for <see cref="NewMember"/>.</param>
+        public GuildMemberUpdatedNotification(
+            ISocketGuildUser oldMember,
+            ISocketGuildUser newMember)
+        {
+            OldMember = oldMember;
+            NewMember = newMember;
+        }
+
+        /// <summary>
+        /// A model of the Guild Member that was updated, from before the update.
+        /// </summary>
+        public ISocketGuildUser OldMember { get; }
+
+        /// <summary>
+        /// A model of the Guild Member that was updated, from after the update.
+        /// </summary>
+        public ISocketGuildUser NewMember { get; }
+    }
+}
