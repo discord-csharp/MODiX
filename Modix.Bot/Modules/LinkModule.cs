8ea231d392e1e235b335fce9bde7527d4f30120b
Sergio Pedri <sergio0694@live.com>
2020-11-08 14:35:37 +0100

    Add sharplab code preview in embed

--- Modix.Bot/Modules/LinkModule.cs
+++ Modix.Bot/Modules/LinkModule.cs
@@ -32,13 +32,73 @@ namespace Modix.Bot.Modules
                 return;
             }
 
-            await ReplyAsync(embed: new EmbedBuilder()
+            if (host.Equals("sharplab.io") &&
+                TryPrepareSharplabPreview(uri.OriginalString, out string preview))
+            {
+                string markdownUrl = Format.Url($"{host} (click here)", uri.ToString());
+                string markdownText = $"{markdownUrl}\r{preview}";
+
+                await ReplyAsync(embed: new EmbedBuilder()
+                    .WithDescription(markdownText)
+                    .WithUserAsAuthor(Context.User)
+                    .WithColor(Color.LightGrey)
+                    .Build());
+            }
+            else
+            {
+                await ReplyAsync(embed: new EmbedBuilder()
                 .WithDescription(Format.Url($"{host} (click here)", uri.ToString()))
                 .WithUserAsAuthor(Context.User)
                 .WithColor(Color.LightGrey)
                 .Build());
+            }
 
             await Context.Message.DeleteAsync();
+        }
+
+        private static bool TryPrepareSharplabPreview(string url, out string preview)
+        {
+            if (!url.Contains("#v2:"))
+            {
+                preview = null;
+
+                return false;
+            }
+
+            try
+            {
+                // Decode the compressed code from the URL payload
+                string base64Text = Url.Substring(Url.IndexOf("#v2:") + "#v2:".Length);
+                string plainText = LZString.DecompressFromBase64(base64Text);
+
+                // Extract the option and get the target language
+                var textParts = Regex.Match(plainText, @"([^|]*)\|([\s\S]*)$");
+                var languageOption = Regex.Match(textParts.Groups[1].Value, @"l:(\w+)");
+                string language = languageOption.Success ? languageOption.Groups[1].Value : "cs";
+                string sourceCode = textParts.Groups[2].Value;
+
+                // Replace the compression tokens
+                if (language is "cs")
+                {
+                    sourceCode = Regex.Replace(sourceCode, @"@(\d+|@)", match =>
+                    {
+                        if (match.Value is "@@") return "@";
+
+                        return Tokens[int.Parse(match.Groups[1].Value)];
+                    });
+                }
+                else sourceCode = sourceCode.Replace("@@", "@");
+
+                preview = $"```{language}\r{sourceCode}\r```";
+
+                return true;
+            }
+            catch
+            {
+                preview = null;
+
+                return false;
+            }
         }
 
         private static readonly ImmutableArray<string> _allowedHosts
@@ -48,5 +108,36 @@ namespace Modix.Bot.Modules
                 "docs.microsoft.com",
                 "www.docs.microsoft.com"
             );
+
+        private static readonly ImmutableArray<string> _sharplabCSTokens
+            = ImmutableArray.Create(new[]
+            {
+                "using",
+                "System",
+                "class",
+                "public",
+                "void",
+                "Func",
+                "Task",
+                "return",
+                "async",
+                "await",
+                "string",
+                "yield",
+                "Action",
+                "IEnumerable",
+                "System.Collections.Generic",
+                "System.Threading.Tasks",
+                "static",
+                "Program",
+                "Main",
+                "Console.WriteLine",
+                "<help.run.csharp>",
+                "using System;",
+                "public static void Main()",
+                "public static class Program",
+                "Inspect.Allocations(() =>",
+                "Inspect.MemoryGraph("
+            });
     }
 }
