c6be17dc4f54fbcff767710c7d04e81c9c828cd9
Scott Caldwell <scott.michael.caldwell@gmail.com>
2021-11-23 17:44:08 -0500

    - Fix display errors in !link - Add additional defensive checks for description length - Add !sharplab as an alias - Add support for IL links

--- Modix.Services/Utilities/FormatUtilities.cs
+++ Modix.Services/Utilities/FormatUtilities.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Globalization;
 using System.Linq;
 using System.Net;
@@ -226,5 +227,139 @@ namespace Modix.Services.Utilities
 
         private static readonly Regex _containsSpoilerRegex
             = new Regex(@"\|\|.+\|\|", RegexOptions.Compiled);
+
+#nullable enable
+        public static string FormatCodeForEmbed(string language, string sourceCode, int maxLength)
+        {
+            if (maxLength <= 0)
+                return string.Empty;
+
+            // Trim, for good measure
+            sourceCode = sourceCode.Trim();
+            var processedLines = new List<string>();
+            var braceOnlyLinesEliminated = 0;
+            var currentLength = 0;
+
+            var lines = sourceCode
+                .Replace("\r", string.Empty)
+                .Split('\n', StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => x.TrimEnd())
+                .Where(x => !string.IsNullOrEmpty(x))
+                .ToImmutableArray();
+
+            // Embeds always end up being too long and then messy to display in chat. They also disrupt conversations
+            // a lot as they just end up being a huge wall of text out of nowhere. To account for this, we will:
+            //   - Trim all lines at the right side
+            //   - Remove blank lines
+            //   - Change all single-line opening brackets to be on the previous line instead
+            //   - Clip every line to the maximum length in an embed
+            //   - Clip the total number of lines to 10
+            //   - If there's more, add a comment indicating the number of remaining lines
+            foreach (var line in lines)
+            {
+                const int MaxLineLength = 61;
+
+                if (line.Length < MaxLineLength - 1)
+                {
+                    if (line.EndsWith('{') &&
+                        string.IsNullOrWhiteSpace(line[..^1]) &&
+                        processedLines.Count > 0)
+                    {
+                        if (!TryReplaceLine(^1, processedLines[^1] + " {"))
+                        {
+                            AddRemainingLineComment();
+                            break;
+                        }
+                        braceOnlyLinesEliminated++;
+                    }
+                    else if (!TryAddLine(line))
+                    {
+                        AddRemainingLineComment();
+                        break;
+                    }
+                }
+                else
+                {
+                    if (!TryAddLine(line[..(MaxLineLength - 3)] + "..."))
+                    {
+                        AddRemainingLineComment();
+                        break;
+                    }
+                }
+
+                if (processedLines.Count == 10)
+                {
+                    var remainingCount = GetRemainingLineCount();
+
+                    // Might as well just add the last line to the embed,
+                    // since we'd just be adding a "1 more line" line otherwise.
+                    // We'd be adding a line either way, so we should go with the
+                    // more useful option.
+                    if (remainingCount == 1)
+                        continue;
+
+                    if (remainingCount <= 0)
+                        break;
+
+                    AddRemainingLineComment();
+                    break;
+                }
+            }
+
+            var code = string.Join('\n', processedLines);
+            return Format.Code(code, language);
+
+            bool TryAddLine(string line)
+            {
+                var remainingCount = GetRemainingLineCount();
+                var possibleRemainingLineCommentLength = remainingCount > 1 // 1, because the current line is included in the count
+                    ? GetRemainingLineCountComment(remainingCount).Length
+                    : 0;
+
+                if (line.Length + currentLength + possibleRemainingLineCommentLength + 1 > maxLength) // +1 because of the newline that will be added later
+                    return false;
+
+                processedLines.Add(line);
+                currentLength += line.Length + 1; // +1 because of the newline that will be added later
+                return true;
+            }
+
+            bool TryReplaceLine(Index index, string line)
+            {
+                var remainingCount = GetRemainingLineCount();
+                var possibleRemainingLineCommentLength = remainingCount > 1 // 1, because the current line is included in the count
+                    ? GetRemainingLineCountComment(remainingCount).Length
+                    : 0;
+
+                var lengthDifference = line.Length - processedLines[index].Length;
+
+                if (lengthDifference + currentLength + possibleRemainingLineCommentLength > maxLength)
+                    return false;
+
+                processedLines[index] = line;
+                currentLength += lengthDifference;
+                return true;
+            }
+
+            int GetRemainingLineCount()
+                => lines.Length - processedLines.Count - braceOnlyLinesEliminated;
+
+            string GetRemainingLineCountComment(int remainingCount)
+            {
+                var commentStart = language switch
+                {
+                    "vb" => "'",
+                    _ => "//",
+                };
+
+                return $"{commentStart} {remainingCount} more lines. Follow the link to view.";
+            }
+
+            void AddRemainingLineComment()
+            {
+                processedLines.Add(GetRemainingLineCountComment(GetRemainingLineCount()));
+            }
+        }
+#nullable restore
     }
 }
