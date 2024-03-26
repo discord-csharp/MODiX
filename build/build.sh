909e668608135d24598979ddc84fbf8f0238461c
Patrick Klaeren <1341180+patrickklaeren@users.noreply.github.com>
2024-03-26 15:52:04 +0000

    Remove build sh scripts

--- build/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-apt-get update && apt install curl -y
-curl -sL https://deb.nodesource.com/setup_10.x | bash -
-apt-get install nodejs -y
-
-dotnet publish Modix.sln -c Release -r linux-x64 --self-contained -o /app
