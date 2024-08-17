a6e89817b77de482b568cccf95dc3c792e8b83f2
calledude <22471295+calledude@users.noreply.github.com>
2024-08-17 15:19:39 +0200

    Move navbar related components over to the wasm project

--- Modix.Web.Wasm/Program.cs
+++ Modix.Web.Wasm/Program.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Modix.Web.Models;
 using Modix.Web.Wasm.Security;
+using MudBlazor;
+using MudBlazor.Services;
 
 namespace Modix.Web.Wasm;
 public class Program
@@ -9,12 +12,20 @@ public class Program
     {
         var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
+        builder.Services.AddScoped<SessionState>();
+
+        builder.Services.AddCascadingValue(sp => sp.GetRequiredService<SessionState>());
+
         builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
         builder.Services
             .AddAuthorizationCore()
             .AddCascadingAuthenticationState()
             .AddSingleton<AuthenticationStateProvider, PersistentAuthenticationStateProvider>();
+
+        builder.Services
+            .AddMudServices()
+            .AddMudMarkdownServices();
 
         await builder.Build().RunAsync();
     }
