2f5cc4620b60fe536960a4113a8dd76f17d52605
Steven He <hez2010@outlook.com>
2024-11-09 03:41:26 +0900

    Update snippets

--- src/Modix.Bot/Modules/GodboltDisasmModal.cs
+++ src/Modix.Bot/Modules/GodboltDisasmModal.cs
@@ -6,16 +6,30 @@ namespace Modix.Bot.Modules
 {
     public class GodboltDisasmModal : IModal
     {
+        private string _language = "csharp";
+
         public string Title => "Godbolt";
 
         [InputLabel("Code")]
-        [ModalTextInput("code", TextInputStyle.Paragraph, placeholder: "System.Console.WriteLine(42);")]
+        [ModalTextInput("code", TextInputStyle.Paragraph, initValue: """
+            using System;
+
+            class Program
+            {
+                static int Square(int num) => num * num;
+                static void Main() => Console.WriteLine(Square(42));
+            }
+            """)]
         public required string Code { get; set; }
 
         [InputLabel("Language: csharp, fsharp, vb or il")]
         [ModalTextInput("language", initValue: "csharp")]
         [RequiredInput(false)]
-        public required string Language { get; set; }
+        public required string Language
+        {
+            get => _language;
+            set => _language = value.ToLowerInvariant();
+        }
 
         [InputLabel("Arguments")]
         [ModalTextInput("arguments", TextInputStyle.Short)]
